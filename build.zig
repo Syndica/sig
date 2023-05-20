e9645c0ce4823fd49db02053bad673e77cf148b5
ultd <ahmad.git@protonmail.com>
2023-05-20 14:55:06 -0500

    fix to tests

--- build.zig
+++ build.zig
@@ -25,7 +25,7 @@ pub fn build(b: *std.Build) void {
         .name = "sig",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
     });
