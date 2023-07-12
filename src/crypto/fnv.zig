1217ece696212ee1f5e6cd7af7809d02ccd52183
0xNineteen <0x39015319@gmail.com>
2023-07-12 13:09:17 -0400

    add hasher tests

--- src/crypto/fnv.zig
+++ src/crypto/fnv.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 /// ***FnvHasher*** is a FNV-1 (64-bit) hasher implementation.
 /// See: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 pub const FnvHasher = struct {
@@ -9,25 +11,25 @@ pub const FnvHasher = struct {
 
     pub fn init() Self {
         return Self{
-            .value = DEFAULT_OFFSET,
+            .hashh = DEFAULT_OFFSET,
         };
     }
 
     pub fn initWithOffset(offset: u64) Self {
         return Self{
-            .value = offset,
+            .hashh = offset,
         };
     }
 
     pub fn update(self: *Self, input: []const u8) void {
         for (input) |byte| {
-            self.value ^= byte;
-            self.value *%= prime;
+            self.hashh ^= byte;
+            self.hashh *%= prime;
         }
     }
 
     pub fn final(self: *Self) u64 {
-        return self.value;
+        return self.hashh;
     }
 
     pub fn hash(input: []const u8) u64 {
@@ -42,3 +44,21 @@ pub const FnvHasher = struct {
         return c.final();
     }
 };
+
+test "fnv hasher with default is correct" {
+    const exp = 12638152016183539244; 
+
+    var hasher = FnvHasher.init();
+    hasher.hash(&.{ 1 });
+    const result = hasher.final();
+    try std.testing.expectEqual(exp, result);
+}
+
+test "fnv hasher with offset is correct" {
+    const exp = 11233064889143142093; 
+
+    var hasher = FnvHasher.initWithOffset(19);
+    hasher.hash(&.{ 1, 2, 3 });
+    const result = hasher.final();
+    try std.testing.expectEqual(exp, result);
+}
