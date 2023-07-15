3eebbf914cc49fc188bf3af8c72d651acea68254
0xNineteen <0x39015319@gmail.com>
2023-07-15 16:18:47 -0400

    update bloom api

--- src/bloom/bloom.zig
+++ src/bloom/bloom.zig
@@ -21,10 +21,10 @@ pub const Bloom = struct {
 
     const Self = @This();
 
-    pub fn init(allocator: std.mem.Allocator) Self {
+    pub fn init(allocator: std.mem.Allocator, num_bits: u64, keys: ArrayList(u64)) Self {
         return Self{
-            .keys = ArrayList(u64).init(allocator),
-            .bits = DynamicBitSet.initEmpty(allocator, 0) catch unreachable,
+            .keys = keys,
+            .bits = DynamicBitSet.initEmpty(allocator, num_bits) catch unreachable,
             .num_bits_set = 0,
         };
     }
@@ -34,9 +34,9 @@ pub const Bloom = struct {
         self.keys.deinit();
     }
 
-    pub fn add(self: *Self, key: u64) void {
-        for (self.keys.items) |k| {
-            var i = self.pos(key, k);
+    pub fn add(self: *Self, key: []const u8) void {
+        for (self.keys.items) |hash_index| {
+            var i = self.pos(key, hash_index);
             if (!self.bits.isSet(i)) {
                 self.num_bits_set +|= 1;
                 self.bits.set(i);
@@ -44,28 +44,15 @@ pub const Bloom = struct {
         }
     }
 
-    pub fn pos(self: *Self, key: u64, k: u64) u64 {
-        return self.hash_at_index(key, k) % @bitSizeOf(u64);
+    pub fn pos(self: *Self, bytes: []const u8, hash_index: u64) u64 {
+        return hash_at_index(bytes, hash_index) % @as(u64, self.bits.capacity());
     }
 
-    pub fn hash_at_index(key: u64, hash_index: u64) u64 {
-        var bytes = std.mem.asBytes(&key);
+    pub fn hash_at_index(bytes: []const u8, hash_index: u64) u64 {
         var hasher = FnvHasher.initWithOffset(hash_index);
         hasher.update(bytes);
         return hasher.final();
     }
-
-    // pub fn pos()
-
-    // pub fn add(self: *Self) void {
-    //      for (self.keys) |key| {
-    //         let pos = self.pos(key, *k);
-    //         if !self.bits.get(pos) {
-    //             self.num_bits_set = self.num_bits_set.saturating_add(1);
-    //             self.bits.set(pos, true);
-    //         }
-    //     }
-    // }
 };
 
 fn bincode_serialize_bit_vec(writer: anytype, data: anytype, params: bincode.Params) !void {
@@ -80,18 +67,41 @@ fn bincode_deserialize_bit_vec(allocator: std.mem.Allocator, comptime T: type, r
     return try bitvec.toBitSet(allocator);
 }
 
-// TODO: Finish test
-test "bloom serializes/deserializes correctly" {
+// // TODO: Finish test
+// test "bloom serializes/deserializes correctly" {
+//     var keys = ArrayList(u64).init(testing.allocator);
+//     _ = keys;
+//     var bloom = Bloom.init(testing.allocator);
+//     var buf: [10000]u8 = undefined;
+
+//     var out = try bincode.writeToSlice(buf[0..], bloom, bincode.Params.standard);
+
+
+//     std.log.debug("out: {any}", .{out});
+
+//     var deserialized = try bincode.readFromSlice(testing.allocator, Bloom, out, bincode.Params.standard);
+
+//     try testing.expect(bloom.num_bits_set == deserialized.num_bits_set);
+// }
+
+test "compare bloom to rust" {
     var keys = ArrayList(u64).init(testing.allocator);
-    _ = keys;
-    var bloom = Bloom.init(testing.allocator);
+    try keys.append(1);
+    try keys.append(2);
+    try keys.append(3);
+
+    var bloom = Bloom.init(testing.allocator, 100, keys);
     var buf: [10000]u8 = undefined;
 
-    var out = try bincode.writeToSlice(buf[0..], bloom, bincode.Params.standard);
+    const v: [1]u8 = .{ 1 };
+    bloom.add(&v);
 
-    std.log.debug("out: {any}", .{out});
+    var bytes = try bincode.writeToSlice(buf[0..], bloom, bincode.Params.standard);
 
-    var deserialized = try bincode.readFromSlice(testing.allocator, Bloom, out, bincode.Params.standard);
+    // let mut bloom = Bloom::new(100, vec![1, 2, 3]);
+    // bloom.add(&[1u8; 1]);
+    // println!("{:?}", bincode::serialize(&bloom).unwrap());
+    const rust_bytes = .{3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 64, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0}; 
 
-    try testing.expect(bloom.num_bits_set == deserialized.num_bits_set);
+    try testing.expectEqualSlices(u8, bytes[0..bytes.len], &rust_bytes);
 }
