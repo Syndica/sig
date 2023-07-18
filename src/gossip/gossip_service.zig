cf2af645c01b73ac7ce37c9ae2f5af99524a31ea
0xNineteen <0x39015319@gmail.com>
2023-07-18 13:55:31 -0400

    add pong verification

--- src/gossip/gossip_service.zig
+++ src/gossip/gossip_service.zig
@@ -168,7 +168,21 @@ pub const GossipService = struct {
             // }
 
             var protocol_message = try bincode.readFromSlice(allocator, Protocol, p.data[0..p.size], bincode.Params.standard);
-            logger.debugf("got a protocol message: {any}", .{protocol_message});
+            switch (protocol_message) {
+                .PongMessage => |pong| {
+                    // verification 
+                    var pong_hash = pong.hash.data; 
+                    const valid_pong = pong.signature.verify(pong.from, &pong_hash) catch false;
+                    if (valid_pong) {
+                        logger.debugf("got a pong message", .{});
+                    } else { 
+                        logger.debugf("pong message verification failed...", .{});
+                    }
+                }, 
+                else => { 
+                    logger.debugf("got a protocol message: {any}", .{protocol_message});
+                }
+            }
         }
     }
 };
