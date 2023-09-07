545abf3bda916bc9ee0bc2bb4f72fa96a2e32143
x19 <0x39015319@gmail.com>
2023-09-07 17:31:12 -0400

    update

--- src/benchmarks.zig
+++ src/benchmarks.zig
@@ -6,38 +6,9 @@ const BENCHMARK_FLAG = "benchmark_";
 
 pub fn main() !void {
     try benchmark(@import("gossip/socket_utils.zig").benchmark_packet_processing);
-
-    // const BenchmarkFnc = struct { type, []const u8 };
-
-    // const benchmark_fcns = comptime blk: {
-    //     var benchmark_fcns: []const BenchmarkFnc = &[_]BenchmarkFnc{};
-
-    //     for (std.meta.declarations(lib)) |decl| {
-    //         const pkg = @field(lib, decl.name);
-    //         for (std.meta.declarations(pkg)) |pkg_decl| {
-    //             // if benchmark_ ...
-    //             if (std.mem.startsWith(u8, pkg_decl.name, BENCHMARK_FLAG)) {
-    //                 const bench = @field(@field(lib, decl.name), pkg_decl.name);
-    //                 benchmark_fcns = benchmark_fcns ++ &[_]BenchmarkFnc{.{ bench, decl.name ++ "." ++ pkg_decl.name }};
-    //             }
-    //         }
-    //     }
-    //     break :blk benchmark_fcns;
-    // };
-
-    // for (benchmark_fcns) |f| {
-    //     const fcn = f[0];
-    //     const fcn_name = f[1];
-    //     std.debug.print("Running benchmark: {s}\n", .{fcn_name});
-    //     _ = fcn;
-
-    //     // var timer = try std.time.Timer.start();
-    //     // fcn();
-    //     // const elapsed = timer.read();
-    //     // std.debug.print("=> Took: {d}ms\n", .{elapsed / std.time.ns_per_ms});
-    // }
 }
 
+// src: https://github.com/Hejsil/zig-bench
 // bench.zig
 const debug = std.debug;
 const io = std.io;
