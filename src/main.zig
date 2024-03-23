b27a6fa89da3ab2bfae5df411227aa9e5554a46b
ultd <ahmad.git@protonmail.com>
2024-03-23 10:17:43 -0500

    disable imported libs debug logs

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,12 @@
 const cmd = @import("cmd/cmd.zig");
+const std = @import("std");
+
+// We set this so that std.log knows not to log .debug level messages
+// which libraries we import will use
+pub const std_options = struct {
+    // Set the log level to info
+    pub const log_level = .info;
+};
 
 pub fn main() !void {
     try cmd.run();
