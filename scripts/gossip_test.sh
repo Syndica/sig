d805ea2347f3163b02d3dd47acaddb590388e100
x19 <0x39015319@gmail.com>
2024-06-04 15:21:29 -0400

    init

---
+++ scripts/gossip_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Running gossip test for $1 seconds"
+
+# build and run gossip
+zig build
+./zig-out/bin/sig gossip -e entrypoint.testnet.solana.com:8001 -e entrypoint2.testnet.solana.com:8001 &
+
+# Get the process ID of the last background command
+PID=$!
+
+# Sleep for 30 seconds
+sleep $1
+
+# Kill the process
+kill $PID
