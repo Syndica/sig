2e7e3ac0d1841b4274445826f17ef3ebbe946d48
x19 <0x39015319@gmail.com>
2024-06-05 13:30:15 -0400

    fix gossip ci

--- scripts/gossip_test.sh
+++ scripts/gossip_test.sh
@@ -19,7 +19,6 @@ fi
 echo "Running gossip test for $1 seconds"
 
 # build and run gossip
-zig build -Doptimize=ReleaseSafe && \
 ./zig-out/bin/sig gossip \
     -e entrypoint.testnet.solana.com:8001 \
     -e entrypoint2.testnet.solana.com:8001 2>&1 &
