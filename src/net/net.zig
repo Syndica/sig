70cc91d450c75b40c54aa942d5818cde4d2fc69d
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2024-06-10 20:59:16 +0200

    Pass around []const T instead of ArrayList & also add better toString methods to SocketAddr

--- src/net/net.zig
+++ src/net/net.zig
@@ -240,9 +240,19 @@ pub const SocketAddr = union(enum(u8)) {
     /// - integer: length of the string within the array
     pub fn toString(self: Self) struct { [53]u8, usize } {
         var buf: [53]u8 = undefined;
-        var stream = std.io.fixedBufferStream(&buf);
+        const len = self.toStringBuf(&buf);
+        return .{ buf, len };
+    }
+
+    pub fn toStringBounded(self: Self) std.BoundedArray(u8, 53) {
+        var buf: [53]u8 = undefined;
+        return std.BoundedArray(u8, 53).fromSlice(buf[0..self.toStringBuf(&buf)]) catch unreachable;
+    }
+
+    pub fn toStringBuf(self: Self, buf: *[53]u8) std.math.IntFittingRange(0, 53) {
+        var stream = std.io.fixedBufferStream(buf);
         self.toAddress().format("", .{}, stream.writer()) catch unreachable;
-        return .{ buf, stream.pos };
+        return @intCast(stream.pos);
     }
 
     pub fn isUnspecified(self: *const Self) bool {
