2d6a4ba27dfeda336597c4da94d52fcdb79c87a2
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2024-06-27 17:11:02 +0200

    LogLevel CLI arg improvement Remove consequently unused `enumFromName` utility function & amend some imports in the process.

--- src/utils/types.zig
+++ src/utils/types.zig
@@ -2,17 +2,6 @@
 
 const std = @import("std");
 
-/// Given the string name of an enum variant,
-/// return the instance of the enum with that name.
-pub fn enumFromName(comptime T: type, variant_name: []const u8) error{UnknownVariant}!T {
-    inline for (@typeInfo(T).Enum.fields) |field| {
-        if (std.mem.eql(u8, field.name, variant_name)) {
-            return @enumFromInt(field.value);
-        }
-    }
-    return error.UnknownVariant;
-}
-
 /// Tuple type representing the args of a function. This is
 /// the type you are required to pass into the @call builtin.
 ///
