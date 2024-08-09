8b93fa92ed066a8cb70bc80745f33a2951cbfc3c
Drew Nutter <dnut@users.noreply.github.com>
2024-08-09 08:39:13 -0400

    docs(blockstore): explain WriteBatch with doc comment

--- src/blockstore/hashmap_db.zig
+++ src/blockstore/hashmap_db.zig
@@ -144,6 +144,16 @@ pub fn SharedHashMapDB(comptime column_families: []const ColumnFamily) type {
             }
         }
 
+        /// A write batch is a sequence of operations that execute atomically.
+        /// This is typically called a "transaction" in most databases.
+        ///
+        /// Use this instead of Database.put or Database.delete when you need
+        /// to ensure that a group of operations are either all executed
+        /// successfully, or none of them are executed.
+        ///
+        /// It is called a write batch instead of a transaction because:
+        /// - rocksdb uses the name "write batch" for this concept
+        /// - this name avoids confusion with solana transactions
         pub const WriteBatch = struct {
             allocator: Allocator,
             instructions: std.ArrayListUnmanaged(Instruction),
--- src/blockstore/rocksdb.zig
+++ src/blockstore/rocksdb.zig
@@ -137,6 +137,16 @@ pub fn RocksDB(comptime column_families: []const ColumnFamily) type {
             return callRocks(self.logger, self.db.write, .{batch.inner});
         }
 
+        /// A write batch is a sequence of operations that execute atomically.
+        /// This is typically called a "transaction" in most databases.
+        ///
+        /// Use this instead of Database.put or Database.delete when you need
+        /// to ensure that a group of operations are either all executed
+        /// successfully, or none of them are executed.
+        ///
+        /// It is called a write batch instead of a transaction because:
+        /// - rocksdb uses the name "write batch" for this concept
+        /// - this name avoids confusion with solana transactions
         pub const WriteBatch = struct {
             inner: rocks.WriteBatch,
             cf_handles: []const rocks.ColumnFamilyHandle,
