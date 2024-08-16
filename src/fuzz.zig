6a3d2be4ed776ac4c730c62d5ec3a460bbddc4d0
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2024-08-16 18:21:25 +0400

    Improvement to fuzz filter cli input
    Signed-off-by: oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>

--- src/fuzz.zig
+++ src/fuzz.zig
@@ -10,6 +10,15 @@ const logger = lib.trace.log;
 // where seeds are saved (in case of too many logs)
 const SEED_FILE_PATH = "test_data/fuzz_seeds.txt";
 
+// Supported fuzz filters.
+// NOTE: changing these enum variants will require a change to the fuzz/kcov in `scripts/`
+pub const FuzzFilter = enum {
+    accountsdb,
+    snapshot,
+    gossip_service,
+    gossip_table,
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -22,8 +31,9 @@ pub fn main() !void {
     const maybe_filter = cli_args.next();
     const filter = blk: {
         if (maybe_filter) |filter| {
+            const parsed_filter = std.meta.stringToEnum(FuzzFilter, filter) orelse return error.UnknownFilter;
             std.debug.print("filtering fuzz testing with prefix: {s}\n", .{filter});
-            break :blk filter;
+            break :blk parsed_filter;
         } else {
             std.debug.print("fuzz filter required: usage: zig build fuzz -- <filter>\n", .{});
             return error.NoFilterProvided;
@@ -42,23 +52,16 @@ pub fn main() !void {
     std.debug.print("using seed: {d}\n", .{seed});
     try writeSeedToFile(filter, seed);
 
-    // NOTE: changing these hardcoded str values will require a change to the fuzz/kcov in `scripts/`
-    if (std.mem.startsWith(u8, filter, "accountsdb")) {
-        try accountsdb_fuzz.run(seed, &cli_args);
-    } else if (std.mem.startsWith(u8, filter, "snapshot")) {
-        try accountsdb_snapshot_fuzz.run(&cli_args);
-    } else if (std.mem.startsWith(u8, filter, "gossip_service")) {
-        try gossip_fuzz_service.run(seed, &cli_args);
-    } else if (std.mem.startsWith(u8, filter, "gossip_table")) {
-        try gossip_fuzz_table.run(seed, &cli_args);
-    } else {
-        std.debug.print("unknown fuzz filter: {s}\n", .{filter});
-        return error.UnknownFilter;
+    switch (filter) {
+        .accountsdb => try accountsdb_fuzz.run(seed, &cli_args),
+        .snapshot => try accountsdb_snapshot_fuzz.run(&cli_args),
+        .gossip_service => try gossip_fuzz_service.run(seed, &cli_args),
+        .gossip_table => try gossip_fuzz_table.run(seed, &cli_args),
     }
 }
 
 /// writes the seed to the defined seed file (defined by SEED_FILE_PATH)
-pub fn writeSeedToFile(filter: []const u8, seed: u64) !void {
+pub fn writeSeedToFile(filter: FuzzFilter, seed: u64) !void {
     const seed_file = try std.fs.cwd().createFile(SEED_FILE_PATH, .{
         .truncate = false,
     });
@@ -66,5 +69,5 @@ pub fn writeSeedToFile(filter: []const u8, seed: u64) !void {
     try seed_file.seekFromEnd(0);
 
     const now: u64 = @intCast(std.time.timestamp());
-    try seed_file.writer().print("{s}: time: {d}, seed: {d}\n", .{ filter, now, seed });
+    try seed_file.writer().print("{s}: time: {d}, seed: {d}\n", .{ @tagName(filter), now, seed });
 }
