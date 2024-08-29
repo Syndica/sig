211ad5858715b359bf1ef28708c9f12e2f3e9922
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2024-08-29 23:46:00 +0400

    Remove chaining implementation

--- src/trace_ng/lib.zig
+++ src/trace_ng/lib.zig
@@ -1,3 +1,2 @@
 pub const log = @import("log.zig");
 pub const logfmt = @import("logfmt.zig");
-pub const Logger = log.ScopedLogger;
--- src/trace_ng/log.zig
+++ src/trace_ng/log.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const Level = @import("level.zig").Level;
-const entry = @import("entry.zig");
 const logfmt = @import("logfmt.zig");
 const sig = @import("../sig.zig");
 // TODO Improve import.
@@ -10,46 +9,10 @@ const Allocator = std.mem.Allocator;
 const AtomicBool = std.atomic.Value(bool);
 const RecycleFBA = sig.utils.allocators.RecycleFBA;
 
-const Entry = entry.Entry;
-const StdEntry = entry.StdEntry;
-const NoopEntry = entry.NoopEntry;
-
 pub const LogConfig = struct {
     level: Level = Level.debug,
     buff_size: usize = 64,
 };
-
-// // Start: Trying out polymorphism via vtable.
-// const LoggerInterface = struct {
-//     // pointer to the logger object
-//     ptr: *anyopaque,
-//     infoFn: *const fn (ptr: *anyopaque) Entry,
-//     // infoFn: fn (*anyopaque) Entry,
-//     pub fn info(self: LoggerInterface) Entry {
-//         return self.infoFn(self.ptr);
-//     }
-// };
-
-// const StandardLogger = struct {
-//     max_level: Level,
-//     pub fn init(max_level: Level) StandardLogger {
-//         return .{
-//             .max_level = max_level,
-//         };
-//     }
-//     pub fn logger(self: *StandardLogger) LoggerInterface {
-//         return LoggerInterface{
-//             .ptr = self,
-//             .infoFn = info,
-//         };
-//     }
-
-//     pub fn info(ptr: *anyopaque) Entry {
-//         std.debug.print("{s}", .{"Hello World"});
-//         const self: *StandardLogger = @ptrCast(@alignCast(ptr));
-//         return Entry{ .standard = StdEntry.init(@This(), self.max_level) };
-//     }
-// };
 
 pub const Config = struct {
     level: Level = Level.debug,
@@ -59,7 +22,7 @@ pub const Config = struct {
 
 const INITIAL_LOG_CHANNEL_SIZE: usize = 1024;
 
-const UnScoppedLogger = StandardLogger(null);
+const Logger = StandardLogger(null);
 pub fn StandardLogger(comptime scope: ?type) type {
     return struct {
         const Self = @This();
@@ -222,89 +185,40 @@ pub fn StandardLogger(comptime scope: ?type) type {
     };
 }
 
-const Logger = ScopedLogger(null);
-pub fn ScopedLogger(comptime scope: ?type) type {
-    return struct {
-        max_level: Level,
+// const Stuff = struct {
+//     logger: StandardLogger(@This()),
 
-        const Self = @This();
+//     pub fn init(logger: Logger) @This() {
+//         return .{ .logger = logger.withScope(@This()) };
+//     }
 
-        pub fn init(config: LogConfig) Self {
-            return .{ .max_level = config.level };
-        }
+//     pub fn doStuff(self: @This()) void {
+//         self.logger.info().log("doing stuff");
+//         const child = StuffChild.init(self.logger.unscoped());
+//         child.doStuffDetails();
+//     }
+// };
 
-        fn unscoped(self: @This()) Logger {
-            return .{ .max_level = self.max_level };
-        }
+// const StuffChild = struct {
+//     logger: ScopedLogger(@This()),
 
-        fn withScope(self: @This(), comptime new_scope: anytype) ScopedLogger(new_scope) {
-            return .{ .max_level = self.max_level };
-        }
+//     pub fn init(logger: Logger) @This() {
+//         return .{ .logger = logger.withScope(@This()) };
+//     }
 
-        pub fn info(self: @This()) Entry {
-            if (@intFromEnum(self.max_level) >= @intFromEnum(Level.info)) {
-                return Entry{ .standard = StdEntry.init(scope, Level.info) };
-            }
-            return Entry{ .noop = NoopEntry{} };
-        }
-    };
-}
+//     pub fn doStuffDetails(self: @This()) void {
+//         self.logger.info().log("doing stuff details");
+//     }
+// };
 
-const Stuff = struct {
-    logger: ScopedLogger(@This()),
+// test "trace_ng: scope switch" {
+//     const logger: Logger = Logger.init(.{});
+//     logger.info().log("starting the app");
+//     const stuff = Stuff.init(logger);
+//     stuff.doStuff();
+// }
 
-    pub fn init(logger: Logger) @This() {
-        return .{ .logger = logger.withScope(@This()) };
-    }
-
-    pub fn doStuff(self: @This()) void {
-        self.logger.info().log("doing stuff");
-        const child = StuffChild.init(self.logger.unscoped());
-        child.doStuffDetails();
-    }
-};
-
-const StuffChild = struct {
-    logger: ScopedLogger(@This()),
-
-    pub fn init(logger: Logger) @This() {
-        return .{ .logger = logger.withScope(@This()) };
-    }
-
-    pub fn doStuffDetails(self: @This()) void {
-        self.logger.info().log("doing stuff details");
-    }
-};
-
-test "trace_ng: scope switch" {
-    const logger: Logger = Logger.init(.{});
-    logger.info().log("starting the app");
-    const stuff = Stuff.init(logger);
-    stuff.doStuff();
-}
-
-test "trace_ng: chaining" {
-    //var logger = Logger.init(.{});
-    var logger = ScopedLogger(@This()).init(.{});
-    logger.info()
-        .add("f_agent", "firefox")
-        .add("f_version", "v2")
-        .log("Hello Logger");
-
-    logger.info()
-        .log("Hello Logger");
-}
-
-test "trace_ng: multiple methods" {
-    // const buffer_size = 1 * 1024 * 1024; // 1MB
-    // var buffer: [buffer_size]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
-
-    //     level: Level = Level.debug,
-    //    allocator: std.mem.Allocator,
-    //    fba_bytes: u64,
-
+test "trace_ng" {
     // TODO switch to testing allocator and fix any leaks.
     const allocator = std.heap.page_allocator;
 
