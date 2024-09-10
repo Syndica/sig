434b64197be94931c917ea58fa88282b580f4911
Drew Nutter <dnut@users.noreply.github.com>
2024-09-09 20:23:16 -0400

    fix(blockstore): remove stray blockstore file
    the blockstore folder was renamed to ledger. somehow `blockstore/blockstore.zig` snuck its way back in the repo. it's no longer in use, since `ledger/blockstore.zig` has replaced it

--- src/blockstore/blockstore.zig
+++
@@ -1,7 +0,0 @@
-const sig = @import("../sig.zig");
-
-pub const BlockstoreDB = sig.blockstore.rocksdb.RocksDB(&sig.blockstore.schema.list);
-
-test BlockstoreDB {
-    sig.blockstore.database.assertIsDatabase(BlockstoreDB);
-}
