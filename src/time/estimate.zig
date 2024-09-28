adf356b633b8f2ebe68233d5229cd2babf114d02
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2024-09-28 11:30:10 +0400

    clean up in estimate.zig

--- src/time/estimate.zig
+++ src/time/estimate.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const sig = @import("../sig.zig");
 const Logger = sig.trace.Logger;
-// const Logger = sig.trace.Logger;
 
 // TODO: change to writer interface when logger has improved
 pub fn printTimeEstimate(
@@ -13,56 +12,52 @@ pub fn printTimeEstimate(
     comptime name: []const u8,
     other_info: ?[]const u8,
 ) void {
-    _ = logger;
-    _ = name;
     if (i == 0 or total == 0) return;
     if (i > total) {
-        if (other_info) |_| {
-            // logger.infof("{s} [{s}]: {d}/{d} (?%) (est: ? elp: {s})", .{
-            //     name,
-            //     info,
-            //     i,
-            //     total,
-            //     timer.read(),
-            // });
+        if (other_info) |info| {
+            logger.infof("{s} [{s}]: {d}/{d} (?%) (est: ? elp: {s})", .{
+                name,
+                info,
+                i,
+                total,
+                timer.read(),
+            });
         } else {
-            // logger.infof("{s}: {d}/{d} (?%) (est: ? elp: {s})", .{
-            //     name,
-            //     i,
-            //     total,
-            //     timer.read(),
-            // });
+            logger.infof("{s}: {d}/{d} (?%) (est: ? elp: {s})", .{
+                name,
+                i,
+                total,
+                timer.read(),
+            });
         }
         return;
     }
 
     const p_done = i * 100 / total;
-    _ = p_done;
     const left = total - i;
 
     const elapsed = timer.read().asNanos();
     const ns_per_vec = elapsed / i;
     const ns_left = ns_per_vec * left;
-    _ = ns_left;
 
-    if (other_info) |_| {
-        // logger.infof("{s} [{s}]: {d}/{d} ({d}%) (est: {s} elp: {s})", .{
-        //     name,
-        //     info,
-        //     i,
-        //     total,
-        //     p_done,
-        //     std.fmt.fmtDuration(ns_left),
-        //     timer.read(),
-        // });
+    if (other_info) |info| {
+        logger.infof("{s} [{s}]: {d}/{d} ({d}%) (est: {s} elp: {s})", .{
+            name,
+            info,
+            i,
+            total,
+            p_done,
+            std.fmt.fmtDuration(ns_left),
+            timer.read(),
+        });
     } else {
-        // logger.infof("{s}: {d}/{d} ({d}%) (est: {s} elp: {s})", .{
-        //     name,
-        //     i,
-        //     total,
-        //     p_done,
-        //     std.fmt.fmtDuration(ns_left),
-        //     timer.read(),
-        // });
+        logger.infof("{s}: {d}/{d} ({d}%) (est: {s} elp: {s})", .{
+            name,
+            i,
+            total,
+            p_done,
+            std.fmt.fmtDuration(ns_left),
+            timer.read(),
+        });
     }
 }
