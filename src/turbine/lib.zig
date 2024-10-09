76406e767b992b807ed3cbb4be9379ae8072e8e9
Harold Newman <hnewman2744@gmail.com>
2024-10-09 13:50:25 +1100

    Resign changes

--- src/turbine/lib.zig
+++ src/turbine/lib.zig
@@ -1,8 +1,7 @@
-pub const retransmit = @import("retransmit.zig");
+pub const retransmit_service = @import("retransmit_service.zig");
 pub const shred_deduper = @import("shred_deduper.zig");
 pub const turbine_tree = @import("turbine_tree.zig");
 
+pub const ShredDeduper = shred_deduper.ShredDeduper;
 pub const TurbineTree = turbine_tree.TurbineTree;
-pub const TurbineTreeCache = turbine_tree.TurbineTreeCache;
-
-pub const runRetransmitService = retransmit.runRetransmitService;
+pub const TurbineTreeCache = turbine_tree.TurbineTreeProvider;
