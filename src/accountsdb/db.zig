7c6082097baa205093269e190bb0f69fb99f5477
x19 <100000306+0xNineteen@users.noreply.github.com>
2024-10-15 16:13:04 -0400

    fix: path allocations

--- src/accountsdb/db.zig
+++ src/accountsdb/db.zig
@@ -4233,6 +4233,7 @@ pub const BenchmarkAccountsDB = struct {
                 std.fs.cwd().deleteFile(filepath) catch {
                     std.debug.print("failed to delete file: {s}\n", .{filepath});
                 };
+                allocator.free(filepath);
             }
         }
 
@@ -4324,7 +4325,7 @@ pub const BenchmarkAccountsDB = struct {
                     // to be indexed later (and timed)
                     account_files.appendAssumeCapacity(account_file);
                 }
-                all_filenames.appendAssumeCapacity(filepath);
+                all_filenames.appendAssumeCapacity(try allocator.dupe(u8, filepath));
             }
 
             var timer = try sig.time.Timer.start();
