aa310247b1deefc148b0f301e4445af60fe033f5
x19 <100000306+0xNineteen@users.noreply.github.com>
2024-10-16 07:46:23 -0400

    fix: leaks in read/write test

--- src/accountsdb/db.zig
+++ src/accountsdb/db.zig
@@ -4108,7 +4108,7 @@ pub const BenchmarkAccountsDBSnapshotLoad = struct {
 
 pub const BenchmarkAccountsDB = struct {
     pub const min_iterations = 1;
-    pub const max_iterations = 10;
+    pub const max_iterations = 500;
 
     pub const MemoryType = enum {
         ram,
@@ -4281,7 +4281,8 @@ pub const BenchmarkAccountsDB = struct {
         const slot_list_len = bench_args.slot_list_len;
         const total_n_accounts = n_accounts * slot_list_len;
 
-        const allocator = std.heap.c_allocator;
+        // make sure theres no leaks
+        const allocator = if (builtin.is_test) std.testing.allocator else std.heap.c_allocator;
         const disk_path = sig.TEST_DATA_DIR ++ "tmp/";
         std.fs.cwd().makeDir(disk_path) catch {};
         defer std.fs.cwd().deleteTreeMinStackSize(disk_path) catch {};
@@ -4320,6 +4321,10 @@ pub const BenchmarkAccountsDB = struct {
         const write_time = if (bench_args.accounts == .ram) timer_blk: {
             const n_accounts_init = bench_args.n_accounts_multiple * bench_args.n_accounts;
             const accounts = try allocator.alloc(Account, (total_n_accounts + n_accounts_init));
+            defer {
+                for (accounts[0..total_n_accounts]) |account| account.deinit(allocator);
+                allocator.free(accounts);
+            }
             for (0..(total_n_accounts + n_accounts_init)) |i| {
                 accounts[i] = try Account.initRandom(allocator, random, i % 1_000);
             }
@@ -4447,6 +4452,7 @@ pub const BenchmarkAccountsDB = struct {
         while (i < do_read_count) : (i += 1) {
             const pubkey_idx = indexer.sample();
             const account = try accounts_db.getAccount(&pubkeys[pubkey_idx]);
+            defer account.deinit(allocator);
             if (account.data.len != (pubkey_idx % 1_000)) {
                 std.debug.panic("account data len dnm {}: {} != {}", .{ pubkey_idx, account.data.len, (pubkey_idx % 1_000) });
             }
@@ -4477,3 +4483,8 @@ pub const BenchmarkAccountsDB = struct {
         };
     }
 };
+
+test "read/write benchmark" {
+    const arg = BenchmarkAccountsDB.args[0];
+    _ = try BenchmarkAccountsDB.readWriteAccounts(arg);
+}
