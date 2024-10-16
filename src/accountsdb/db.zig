2bf46130d5af6a3685ce54c0002dc0ef4d57f55c
x19 <100000306+0xNineteen@users.noreply.github.com>
2024-10-16 10:24:28 -0400

    fix benchmarks

--- src/accountsdb/db.zig
+++ src/accountsdb/db.zig
@@ -4108,7 +4108,7 @@ pub const BenchmarkAccountsDBSnapshotLoad = struct {
 
 pub const BenchmarkAccountsDB = struct {
     pub const min_iterations = 1;
-    pub const max_iterations = 500;
+    pub const max_iterations = 5;
 
     pub const MemoryType = enum {
         ram,
@@ -4484,7 +4484,20 @@ pub const BenchmarkAccountsDB = struct {
     }
 };
 
-test "read/write benchmark" {
-    const arg = BenchmarkAccountsDB.args[0];
-    _ = try BenchmarkAccountsDB.readWriteAccounts(arg);
+test "read/write benchmark ram" {
+    _ = try BenchmarkAccountsDB.readWriteAccounts(.{
+        .n_accounts = 100_000,
+        .slot_list_len = 1,
+        .accounts = .ram,
+        .index = .ram,
+    });
+}
+
+test "read/write benchmark disk" {
+    _ = try BenchmarkAccountsDB.readWriteAccounts(.{
+        .n_accounts = 100_000,
+        .slot_list_len = 1,
+        .accounts = .disk,
+        .index = .disk,
+    });
 }
