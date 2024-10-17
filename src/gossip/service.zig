91f06d07ba180eb8533abe9ce6320fc7261c9511
x19 <100000306+0xNineteen@users.noreply.github.com>
2024-10-17 17:42:23 -0400

    fix: lint

--- src/gossip/service.zig
+++ src/gossip/service.zig
@@ -2563,10 +2563,7 @@ test "handle pull request" {
         &counter,
         logger,
     );
-    defer { 
-        gossip_service.shutdown();
-        gossip_service.deinit();
-    }
+    defer gossip_service.deinit();
 
     // insert random values
     const N_FILTER_BITS = 1;
