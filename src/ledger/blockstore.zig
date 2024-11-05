876eb2def13c70bb4c489afc28e7323d170e2d96
Drew Nutter <dnut@users.noreply.github.com>
2024-11-05 11:06:54 -0500

    refactor(ledger): extract out import to top

--- src/ledger/blockstore.zig
+++ src/ledger/blockstore.zig
@@ -1,6 +1,7 @@
+const build_options = @import("build-options");
 const ledger = @import("lib.zig");
 
-pub const BlockstoreDB = switch (@import("build-options").blockstore_db) {
+pub const BlockstoreDB = switch (build_options.blockstore_db) {
     .rocksdb => ledger.database.RocksDB(&ledger.schema.list),
     .hashmap => ledger.database.SharedHashMapDB(&ledger.schema.list),
     .lmdb => ledger.database.LMDB(&ledger.schema.list),
