4c7ee534b88e46d49bfe49711c228d96b96fe82a
dadepo <272535+dadepo@users.noreply.github.com>
2024-11-07 00:32:28 +0400

    feat(ledger): bench getting code shreds (#354)
    * fix(ledger): backward merkle root chaining bugs
    
    It was ignoring result of checking chaining, and just always marked it as a duplicate (as if the check failed), and always returned true (as if the check succeeded). complete nonsense
    
    It was comparing chained root to chained root, which is wrong. it should be chained to actual root
    
    This change also refactors common logic into a single place.
    
    Also this now returns an error if the merkle root is missing, because that means there is a severe bug in the validator and it needs to be shutdown and patched immediately.
    
    * Update src/ledger/shred_inserter/merkle_root_checks.zig
    
    Co-authored-by: InKryption <59504965+InKryption@users.noreply.github.com>
    
    * fix(ledger): zig fmt
    
    * refactor(ledger): merkle root validator struct
    
    the four methods all have basically the same dependencies so this bundles them together to clarify their purpose and simplify their expression
    
    * refactor(ledger): use working stores to clarify and minimize dependencies, and to organize working state code
    
    * refactor(ledger): in merkle root checks, rename early and late shreds to old and new shreds
    
    * Added code shreds
    
    * Add BlockstoreReader.getCodeShred
    
    * Bench reader.getCodeShredsForSlot
    
    ---------
    
    Co-authored-by: Drew Nutter <dnut@users.noreply.github.com>
    Co-authored-by: InKryption <59504965+InKryption@users.noreply.github.com>

--- src/ledger/benchmarks.zig
+++ src/ledger/benchmarks.zig
@@ -201,6 +201,62 @@ pub const BenchmarkLedger = struct {
         return duration;
     }
 
+    pub fn @"BlockstoreReader.getCodeShred"() !sig.time.Duration {
+        const allocator = std.heap.c_allocator;
+        var state = try TestState.init(allocator, @src(), .noop);
+        defer state.deinit();
+        var inserter = try state.shredInserter();
+        var reader = try state.reader();
+
+        const shreds_path = "agave.blockstore.bench_read.code_shreds.bin";
+        const shreds = try testShreds(std.heap.c_allocator, shreds_path);
+        defer deinitShreds(allocator, shreds);
+
+        const total_shreds = shreds.len;
+        _ = try ledger.shred_inserter.shred_inserter.insertShredsForTest(&inserter, shreds);
+
+        const slot: u32 = 1;
+
+        var rng = std.Random.DefaultPrng.init(100);
+
+        var indices = try std.ArrayList(u32).initCapacity(inserter.allocator, total_shreds);
+        defer indices.deinit();
+        for (total_shreds) |_| {
+            indices.appendAssumeCapacity(rng.random().uintAtMost(u32, @intCast(total_shreds)));
+        }
+
+        var timer = try sig.time.Timer.start();
+        for (indices.items) |shred_index| {
+            _ = try reader.getCodeShred(slot, shred_index) orelse return error.MissingShred;
+        }
+        return timer.read();
+    }
+
+    pub fn @"BlockstoreReader.getCodeShredsForSlot"() !sig.time.Duration {
+        const allocator = std.heap.c_allocator;
+        var state = try TestState.init(allocator, @src(), .noop);
+        defer state.deinit();
+        var inserter = try state.shredInserter();
+        var reader = try state.reader();
+
+        const shreds_path = "agave.blockstore.bench_read.code_shreds.bin";
+        const shreds = try testShreds(std.heap.c_allocator, shreds_path);
+        defer deinitShreds(allocator, shreds);
+
+        _ = try ledger.shred_inserter.shred_inserter.insertShredsForTest(&inserter, shreds);
+
+        const slot = 1;
+        const start_index = 0;
+
+        var timer = try sig.time.Timer.start();
+        const code_shreds = try reader.getCodeShredsForSlot(slot, start_index);
+        const duration = timer.read();
+        try std.testing.expect(code_shreds.items.len > 0);
+        return duration;
+    }
+
+    /// Benchmarks for LedgerResultWriter.
+    ///
     /// Analogous to [bench_write_transaction_status]https://github.com/anza-xyz/agave/blob/ff1b22007c34669768c5b676cac491f580b39e0b/ledger/benches/blockstore.rs#L206
     pub fn @"LedgerResultWriter.writeTransactionStatus"() !sig.time.Duration {
         const Signature = sig.core.Signature;
