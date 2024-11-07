1f7aaddd6432dbd4509a6cb2b1d3e55239bd9a66
David Rubin <daviru007@icloud.com>
2024-11-07 07:43:43 -0800

    another commit

--- build.zig
+++ build.zig
@@ -105,7 +105,6 @@ pub fn build(b: *Build) void {
         .sanitize_thread = enable_tsan,
     });
     b.installArtifact(unit_tests_exe);
-    unit_tests_exe.root_module.addImport("base58-zig", base58_module);
     unit_tests_exe.root_module.addImport("httpz", httpz_mod);
     unit_tests_exe.root_module.addImport("zig-network", zig_network_module);
     unit_tests_exe.root_module.addImport("rocksdb", rocksdb_mod);
