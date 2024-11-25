8164e816ebead8b25cf6fbee77f1e1a2455d6afa
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2024-11-25 20:32:30 +0100

    Simple RPC Server implementation Because MacOS is silly, the blocking/non-blocking nature of the sockets is tricky, so testing requires this relatively simple setup; it's not clear how to make this shut down gracefully on MacOS if in production. Also add `io.narrowAnyWriter` & `io.narrowAnyReader`.

--- src/rpc/lib.zig
+++ src/rpc/lib.zig
@@ -1,8 +1,13 @@
 pub const client = @import("client.zig");
+pub const server = @import("server.zig");
+
 pub const request = @import("request.zig");
 pub const response = @import("response.zig");
 pub const types = @import("types.zig");
 
 pub const Client = client.Client;
+pub const Server = server.Server;
+
+pub const ClusterType = types.ClusterType;
 pub const Request = request.Request;
 pub const Response = response.Response;
