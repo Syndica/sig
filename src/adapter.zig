0dc10a1f7a963259e06ba0b10a1109adb3410f90
Drew Nutter <dnut@users.noreply.github.com>
2024-12-04 23:15:45 -0300

    fix: lifetime issue with staked nodes pointer

--- src/adapter.zig
+++ src/adapter.zig
@@ -1,4 +1,4 @@
-//! Glue code for hooking up application components from distant areas of the code.
+//! Links dependencies with dependents. Connects components from distant regions of the code.
 
 const std = @import("std");
 const sig = @import("sig.zig");
@@ -30,17 +30,17 @@ pub const RpcSlotLeaders = struct {
     }
 
     pub fn slotLeaders(self: *Self) leader_schedule.SlotLeaders {
-        return leader_schedule.SlotLeaders.init(self, slotLeader);
+        return leader_schedule.SlotLeaders.init(self, get);
     }
 
-    fn slotLeader(self: *Self, slot: sig.core.Slot) ?sig.core.Pubkey {
-        return self.slotLeaderFallible(slot) catch |e| {
+    fn get(self: *Self, slot: sig.core.Slot) ?sig.core.Pubkey {
+        return self.getFallible(slot) catch |e| {
             self.logger.err().logf("error getting leader for slot {} - {}", .{ slot, e });
             return null;
         };
     }
 
-    fn slotLeaderFallible(self: *Self, slot: sig.core.Slot) !?sig.core.Pubkey {
+    fn getFallible(self: *Self, slot: sig.core.Slot) !?sig.core.Pubkey {
         if (self.cache.slotLeader(slot)) |leader| {
             return leader;
         }
@@ -66,29 +66,23 @@ pub const RpcStakedNodes = struct {
     const Self = @This();
     const StakedNodes = sig.shred_network.shred_retransmitter.StakedNodes;
     const NodeToStakeMap = StakedNodes.NodeToStakeMap;
-    const Cache = sig.utils.lru.LruCacheCustom(
-        .locking,
+    const Cache = sig.utils.lru.SharedPointerLru(
         sig.core.Epoch,
-        *NodeToStakeMap,
+        NodeToStakeMap,
         Allocator,
-        destroyCacheItem,
+        NodeToStakeMap.deinit,
     );
 
     pub fn init(allocator: Allocator, rpc_client: sig.rpc.Client) !Self {
         return .{
             .allocator = allocator,
             .rpc_client = rpc_client,
-            .cache = try Cache.initWithContext(allocator, 8, allocator),
+            .cache = try Cache.init(allocator, 8, allocator),
         };
     }
 
     pub fn stakedNodes(self: *Self) StakedNodes {
-        return StakedNodes.init(self, get);
-    }
-
-    fn destroyCacheItem(element: *NodeToStakeMap, allocator: std.mem.Allocator) void {
-        element.deinit(allocator);
-        allocator.destroy(element);
+        return StakedNodes.init(self, get, release);
     }
 
     fn get(self: *Self, epoch: sig.core.Epoch) anyerror!*const NodeToStakeMap {
@@ -104,22 +98,18 @@ pub const RpcStakedNodes = struct {
         var staked_nodes = std.AutoArrayHashMap(sig.core.Pubkey, u64).init(self.allocator);
         errdefer staked_nodes.deinit();
         inline for (all_vote_accounts) |vote_accounts| for (vote_accounts) |vote_account| {
-            const node_entry = try staked_nodes
-                .getOrPut(vote_account.nodePubkey);
+            const node_entry = try staked_nodes.getOrPut(vote_account.nodePubkey);
             if (!node_entry.found_existing) {
                 node_entry.value_ptr.* = 0;
             }
             node_entry.value_ptr.* += vote_account.activatedStake;
         };
 
-        const staked_nodes_ptr = try self.allocator.create(NodeToStakeMap);
-        errdefer self.allocator.destroy(staked_nodes_ptr);
-        staked_nodes_ptr.* = staked_nodes.unmanaged;
-        if (self.cache.put(epoch, staked_nodes_ptr)) |old| {
-            destroyCacheItem(old, self.allocator);
-        }
+        return try self.cache.putGet(epoch, staked_nodes.unmanaged);
+    }
 
-        return staked_nodes_ptr;
+    fn release(self: *Self, ptr: *const NodeToStakeMap) void {
+        self.cache.release(ptr);
     }
 };
 
@@ -131,10 +121,12 @@ pub const BankFieldsStakedNodes = struct {
     const StakedNodes = sig.shred_network.shred_retransmitter.StakedNodes;
 
     pub fn stakedNodes(self: *Self) StakedNodes {
-        return StakedNodes.init(self, get);
+        return StakedNodes.init(self, get, release);
     }
 
     fn get(self: *Self, epoch: sig.core.Epoch) anyerror!*const StakedNodes.NodeToStakeMap {
         return try self.bank_fields.getStakedNodes(self.allocator, epoch);
     }
+
+    fn release(_: *Self, _: *const StakedNodes.NodeToStakeMap) void {}
 };
