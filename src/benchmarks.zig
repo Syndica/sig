87c3bcaef943bf86258dd259057e2975c2d5bc76
x19 <100000306+0xNineteen@users.noreply.github.com>
2025-01-21 16:57:43 -0500

    fix(ci): add timeout for snapshot downloads (#506)

--- src/benchmarks.zig
+++ src/benchmarks.zig
@@ -247,6 +247,7 @@ pub fn main() !void {
                         .force_new_snapshot_download = true,
                         .max_number_of_download_attempts = 50,
                         .min_snapshot_download_speed_mbs = 10,
+                        .download_timeout = Duration.fromMinutes(5),
                     },
                 ) catch |err| {
                     switch (err) {
