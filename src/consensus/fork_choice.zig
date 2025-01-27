19e3dc43ea9b2e8dc4db71e868e5108d54c99768
Dade <272535+dadepo@users.noreply.github.com>
2025-01-27 14:34:11 +0000

    Added implementation for markForkInvalidCandidate

--- src/consensus/fork_choice.zig
+++ src/consensus/fork_choice.zig
@@ -10,10 +10,38 @@ const Slot = sig.core.Slot;
 
 const MAX_ROOT_PRINT_SECONDS: u64 = 60 * 60; // 1 hour
 
+const UpdateLabel = enum {
+    Aggregate,
+    MarkValid,
+    MarkInvalid,
+};
+
+const UpdateOperation = union(enum) {
+    Add: u64,
+    MarkValid: Slot,
+    MarkInvalid: Slot,
+    Subtract: u64,
+    Aggregate,
+};
+
+const SlotHashKeyLabel = struct {
+    slot_hash_key: SlotHashKey,
+    label: UpdateLabel,
+    pub fn order(a: SlotHashKeyLabel, b: SlotHashKeyLabel) std.math.Order {
+        return a.slot_hash_key.order(b.slot_hash_key);
+    }
+};
+
+const UpdateOperations = SortedMap(
+    SlotHashKeyLabel,
+    UpdateOperation,
+);
+
+// TODO Switch this with SlotAndHash.
 const SlotHashKey = struct {
     slot: Slot,
     hash: Hash,
-
+    var default = SlotHashKey{ .slot = 0, .hash = Hash.ZEROES };
     pub fn order(a: SlotHashKey, b: SlotHashKey) std.math.Order {
         if (a.slot == b.slot and a.hash.order(&b.hash) == .eq) {
             return .eq;
@@ -55,8 +83,67 @@ pub const ForkInfo = struct {
     is_duplicate_confirmed: bool,
 
     /// Returns if the fork rooted at this node is included in fork choice
-    fn isCandidate(self: ForkInfo) bool {
+    fn isCandidate(self: *const ForkInfo) bool {
         return self.latest_invalid_ancestor == null;
+    }
+
+    fn setDuplicateConfirmed(self: *ForkInfo) void {
+        self.is_duplicate_confirmed = true;
+        self.latest_invalid_ancestor = null;
+    }
+
+    /// Updates the fork info with a newly valid ancestor.
+    /// If the latest invalid ancestor is less than or equal to the newly valid ancestor,
+    /// it clears the latest invalid ancestor.
+    fn updateWithNewlyValidAncestor(
+        self: *ForkInfo,
+        my_key: *const SlotHashKey,
+        newly_valid_ancestor: Slot,
+    ) void {
+        // Check if there is a latest invalid ancestor
+        if (self.latest_invalid_ancestor) |invalid_ancestor| {
+            // If the latest invalid ancestor is less than or equal to the newly valid ancestor,
+            // clear the latest invalid ancestor
+            if (invalid_ancestor <= newly_valid_ancestor) {
+                // TODO change to logger.
+                std.debug.print(
+                    \\ Fork choice for {} clearing latest invalid ancestor  
+                    \\ {} because {} was duplicate confirmed
+                ,
+                    .{ my_key, invalid_ancestor, newly_valid_ancestor },
+                );
+                self.latest_invalid_ancestor = null;
+            }
+        }
+    }
+
+    /// Updates the fork info with a newly invalid ancestor.
+    /// Asserts that the fork is not duplicate confirmed.
+    /// If the newly invalid ancestor is greater than the current latest invalid ancestor,
+    /// updates the latest invalid ancestor.
+    fn updateWithNewlyInvalidAncestor(
+        self: *ForkInfo,
+        my_key: *const SlotHashKey,
+        newly_invalid_ancestor: Slot,
+    ) void {
+        // Should not be marking a duplicate confirmed slot as invalid
+        std.debug.assert(!self.is_duplicate_confirmed);
+
+        // Check if the newly invalid ancestor is greater than the current latest invalid ancestor
+        const should_update = if (self.latest_invalid_ancestor) |invalid_ancestor|
+            newly_invalid_ancestor > invalid_ancestor
+        else
+            true;
+
+        // If the condition is met, update the latest invalid ancestor
+        if (should_update) {
+            // TODO: Switch to logger.
+            std.debug.print(
+                "Fork choice for {} setting latest invalid ancestor from {?} to {}",
+                .{ my_key, self.latest_invalid_ancestor, newly_invalid_ancestor },
+            );
+            self.latest_invalid_ancestor = newly_invalid_ancestor;
+        }
     }
 };
 
@@ -202,6 +289,36 @@ pub const HeaviestSubtreeForkChoice = struct {
         root_fork_info.parent = null;
         self.tree_root = new_root.*;
         self.last_root_time = Instant.now();
+    }
+
+    pub fn markForkInvalidCandidate(self: *Self, invalid_slot_hash_key: *const SlotHashKey) !void {
+        // Get mutable reference to fork info
+        if (self.fork_infos.getPtr(invalid_slot_hash_key.*)) |_| {
+            // Should not be marking duplicate confirmed blocks as invalid candidates
+            // TODO: Re-enable
+            // std.debug.assert(!fork_info.is_duplicate_confirmed);
+
+            var update_operations = UpdateOperations.init(self.allocator);
+            defer update_operations.deinit();
+
+            // Notify all children that a parent was marked as invalid
+            var children_hash_keys = try self.subtreeDiff(
+                invalid_slot_hash_key,
+                &SlotHashKey.default,
+            );
+
+            for (children_hash_keys.keys()) |child_hash_key| {
+                _ = try self.doInsertAggregateOperation(
+                    &update_operations,
+                    UpdateOperation{ .MarkInvalid = invalid_slot_hash_key.slot },
+                    child_hash_key,
+                );
+            }
+
+            // Aggregate across all ancestors to find new best slots excluding this fork
+            try self.insertAggregateOperations(&update_operations, invalid_slot_hash_key.*);
+            self.processUpdateOperations(update_operations);
+        }
     }
 
     /// Updates the fork tree's metadata for ancestors when a new slot (slot_hash_key) is added.
@@ -404,6 +521,272 @@ pub const HeaviestSubtreeForkChoice = struct {
 
         return reachable_set;
     }
+
+    fn doInsertAggregateOperation(
+        _: *Self,
+        update_operations: *UpdateOperations,
+        modify_fork_validity: ?UpdateOperation,
+        slot_hash_key: SlotHashKey,
+    ) !bool {
+        const aggregate_label = SlotHashKeyLabel{
+            .slot_hash_key = slot_hash_key,
+            .label = .Aggregate,
+        };
+
+        if (update_operations.contains(aggregate_label)) {
+            return false;
+        }
+
+        if (modify_fork_validity) |mark_fork_validity| {
+            switch (mark_fork_validity) {
+                .MarkValid => |slot| {
+                    _ = try update_operations.put(
+                        SlotHashKeyLabel{
+                            .slot_hash_key = slot_hash_key,
+                            .label = .MarkValid,
+                        },
+                        UpdateOperation{ .MarkValid = slot },
+                    );
+                },
+                .MarkInvalid => |slot| {
+                    _ = try update_operations.put(
+                        SlotHashKeyLabel{
+                            .slot_hash_key = slot_hash_key,
+                            .label = .MarkInvalid,
+                        },
+                        UpdateOperation{ .MarkInvalid = slot },
+                    );
+                },
+                else => {},
+            }
+        }
+
+        _ = try update_operations.put(aggregate_label, .Aggregate);
+        return true;
+    }
+
+    fn processUpdateOperations(self: *Self, update_operations_: UpdateOperations) void {
+        // Iterate through the update operations from greatest to smallest slot
+        // Sort the map to ensure keys are in order
+
+        var update_operations = update_operations_;
+        const items_result = update_operations.items();
+
+        // Access the fields of the returned struct
+        const keys = items_result.@"0";
+        const values = items_result.@"1";
+
+        // Iterate through the update operations from greatest to smallest slot
+        var i: usize = keys.len;
+        while (i > 0) {
+            i -= 1; // Move backward through the array
+            const slot_hash_key_label = keys[i];
+            const slot_hash_key = slot_hash_key_label.slot_hash_key;
+            const operation = values[i];
+            switch (operation) {
+                .MarkValid => |valid_slot| self.markForkValid(&slot_hash_key, valid_slot),
+                .MarkInvalid => |invalid_slot| self.markForkInvalid(slot_hash_key, invalid_slot),
+                .Aggregate => self.aggregateSlot(slot_hash_key),
+                .Add => |stake| self.addSlotStake(&slot_hash_key, stake),
+                .Subtract => |stake| self.subtractSlotStake(&slot_hash_key, stake),
+            }
+        }
+    }
+
+    fn insertAggregateOperations(
+        self: *Self,
+        update_operations: *UpdateOperations,
+        slot_hash_key: SlotHashKey,
+    ) !void {
+        try self.doInsertAggregateOperationsAcrossAncestors(
+            update_operations,
+            null,
+            slot_hash_key,
+        );
+    }
+
+    fn doInsertAggregateOperationsAcrossAncestors(
+        self: *Self,
+        update_operations: *UpdateOperations,
+        modify_fork_validity: ?UpdateOperation,
+        slot_hash_key: SlotHashKey,
+    ) !void {
+        var parent_iter = self.ancestorIterator(slot_hash_key);
+        while (parent_iter.next()) |parent_slot_hash_key| {
+            if (!try self.doInsertAggregateOperation(
+                update_operations,
+                modify_fork_validity,
+                parent_slot_hash_key,
+            )) {
+                // If this parent was already inserted, we assume all the other parents have also
+                // already been inserted. This is to prevent iterating over the parents multiple times
+                // when we are aggregating leaves that have a lot of shared ancestors
+                break;
+            }
+        }
+    }
+
+    /// Mark that `valid_slot` on the fork starting at `fork_to_modify_key` has been marked
+    /// valid. Note we don't need the hash for `valid_slot` because slot number uniquely
+    /// identifies a node on a single fork.
+    fn markForkValid(self: *Self, fork_to_modify_key: *const SlotHashKey, valid_slot: Slot) void {
+        // Try to get a mutable reference to the fork info
+        if (self.fork_infos.getPtr(fork_to_modify_key.*)) |fork_info_to_modify| {
+            // Update the fork info with the newly valid ancestor
+            fork_info_to_modify.updateWithNewlyValidAncestor(fork_to_modify_key, valid_slot);
+
+            // If the fork's key matches the valid slot, mark it as duplicate confirmed
+            if (fork_to_modify_key.slot == valid_slot) {
+                fork_info_to_modify.is_duplicate_confirmed = true;
+            }
+        }
+    }
+
+    /// Mark that `invalid_slot` on the fork starting at `fork_to_modify_key` has been marked
+    /// invalid. Note we don't need the hash for `invalid_slot` because slot number uniquely
+    /// identifies a node on a single fork.
+    fn markForkInvalid(self: *Self, fork_to_modify_key: SlotHashKey, invalid_slot: Slot) void {
+        // Try to get a mutable reference to the fork info
+        if (self.fork_infos.getPtr(fork_to_modify_key)) |fork_info_to_modify| {
+            // Update the fork info with the newly invalid ancestor
+            fork_info_to_modify.updateWithNewlyInvalidAncestor(&fork_to_modify_key, invalid_slot);
+        }
+    }
+
+    /// Aggregates stake and height information for the subtree rooted at `slot_hash_key`.
+    /// Updates the fork info with the aggregated values.
+    pub fn aggregateSlot(self: *Self, slot_hash_key: SlotHashKey) void {
+        var stake_voted_subtree: u64 = 0;
+        var deepest_child_height: u64 = 0;
+        var best_slot_hash_key: SlotHashKey = slot_hash_key;
+        var deepest_slot_hash_key: SlotHashKey = slot_hash_key;
+        var is_duplicate_confirmed: bool = false;
+
+        // Get the fork info for the given slot_hash_key
+        if (self.fork_infos.getPtr(slot_hash_key)) |fork_info| {
+            stake_voted_subtree = fork_info.stake_voted_at;
+
+            var best_child_stake_voted_subtree: u64 = 0;
+            var best_child_slot_key: SlotHashKey = slot_hash_key;
+            var deepest_child_stake_voted_subtree: u64 = 0;
+            var deepest_child_slot_key: SlotHashKey = slot_hash_key;
+
+            // Iterate over the children of the current fork
+            for (fork_info.children.keys()) |child_key| {
+                const child_fork_info = self.fork_infos.get(child_key) orelse {
+                    std.debug.panic("Child must exist in fork_info map", .{});
+                };
+
+                const child_stake_voted_subtree = child_fork_info.stake_voted_subtree;
+                const child_height = child_fork_info.height;
+                is_duplicate_confirmed = is_duplicate_confirmed or
+                    child_fork_info.is_duplicate_confirmed;
+
+                // Add the child's stake to the subtree stake
+                stake_voted_subtree += child_stake_voted_subtree;
+
+                // Update the best child if the child is a candidate and meets the conditions
+                if (child_fork_info.isCandidate() and
+                    (best_child_slot_key.order(slot_hash_key) == .eq or
+                    child_stake_voted_subtree > best_child_stake_voted_subtree or
+                    (child_stake_voted_subtree == best_child_stake_voted_subtree and
+                    child_key.order(best_child_slot_key) == .lt)))
+                {
+                    best_child_stake_voted_subtree = child_stake_voted_subtree;
+                    best_child_slot_key = child_key;
+                    best_slot_hash_key = child_fork_info.best_slot;
+                }
+
+                // Update the deepest child based on height, stake, and slot key
+                const is_first_child = deepest_child_slot_key.order(slot_hash_key) == .eq;
+                const is_deeper_child = child_height > deepest_child_height;
+                const is_heavier_child =
+                    child_stake_voted_subtree > deepest_child_stake_voted_subtree;
+                const is_earlier_child = child_key.order(deepest_child_slot_key) == .lt;
+
+                if (is_first_child or
+                    is_deeper_child or
+                    (child_height == deepest_child_height and is_heavier_child) or
+                    (child_height == deepest_child_height and
+                    child_stake_voted_subtree == deepest_child_stake_voted_subtree and
+                    is_earlier_child))
+                {
+                    deepest_child_height = child_height;
+                    deepest_child_stake_voted_subtree = child_stake_voted_subtree;
+                    deepest_child_slot_key = child_key;
+                    deepest_slot_hash_key = child_fork_info.deepest_slot;
+                }
+            }
+        } else {
+            // If the fork info does not exist, return early
+            return;
+        }
+
+        // Update the fork info with the aggregated values
+        const fork_info = self.fork_infos.getPtr(slot_hash_key).?;
+        if (is_duplicate_confirmed and !fork_info.is_duplicate_confirmed) {
+            std.log.info(
+                "Fork choice setting {} to duplicate confirmed",
+                .{slot_hash_key},
+            );
+            fork_info.setDuplicateConfirmed();
+        }
+
+        fork_info.stake_voted_subtree = stake_voted_subtree;
+        fork_info.height = deepest_child_height + 1;
+        fork_info.best_slot = best_slot_hash_key;
+        fork_info.deepest_slot = deepest_slot_hash_key;
+    }
+
+    /// Adds `stake` to the stake voted at and stake voted subtree for the fork identified by `slot_hash_key`.
+    pub fn addSlotStake(self: *Self, slot_hash_key: *const SlotHashKey, stake: u64) void {
+        // Try to get a mutable reference to the fork info
+        if (self.fork_infos.getPtr(slot_hash_key.*)) |fork_info| {
+            // Add the stake to the fork's voted stake and subtree stake
+            fork_info.stake_voted_at += stake;
+            fork_info.stake_voted_subtree += stake;
+        }
+    }
+
+    /// Subtracts `stake` from the stake voted at and stake voted subtree for the fork identified by `slot_hash_key`.
+    pub fn subtractSlotStake(self: *Self, slot_hash_key: *const SlotHashKey, stake: u64) void {
+        // Try to get a mutable reference to the fork info
+        if (self.fork_infos.getPtr(slot_hash_key.*)) |fork_info| {
+            // Substract the stake to the fork's voted stake and subtree stake
+            fork_info.stake_voted_at -= stake;
+            fork_info.stake_voted_subtree -= stake;
+        }
+    }
+
+    fn ancestorIterator(self: *Self, start_slot_hash_key: SlotHashKey) AncestorIterator {
+        return AncestorIterator{
+            .current_slot_hash_key = start_slot_hash_key,
+            .fork_infos = &self.fork_infos,
+        };
+    }
+};
+
+const AncestorIterator = struct {
+    current_slot_hash_key: SlotHashKey,
+    fork_infos: *const std.AutoHashMap(SlotHashKey, ForkInfo),
+
+    pub fn init(
+        start_slot_hash_key: SlotHashKey,
+        fork_infos: *const std.AutoHashMap(SlotHashKey, ForkInfo),
+    ) AncestorIterator {
+        return AncestorIterator{
+            .current_slot_hash_key = start_slot_hash_key,
+            .fork_infos = fork_infos,
+        };
+    }
+
+    pub fn next(self: *AncestorIterator) ?SlotHashKey {
+        const fork_info = self.fork_infos.get(self.current_slot_hash_key) orelse return null;
+        const parent_slot_hash_key = fork_info.parent orelse return null;
+
+        self.current_slot_hash_key = parent_slot_hash_key;
+        return self.current_slot_hash_key;
+    }
 };
 
 /// Testing only ensures everything compiles.
@@ -439,6 +822,18 @@ test "HeaviestSubtreeForkChoice.setTreeRoot" {
     defer fc.deinit();
 
     _ = try fc.setTreeRoot(
+        &SlotHashKey{ .slot = 0, .hash = Hash.ZEROES },
+    );
+}
+
+test "HeaviestSubtreeForkChoice.markForkInvalidCandidate" {
+    var fc = try HeaviestSubtreeForkChoice.init(
+        test_allocator,
+        SlotHashKey{ .slot = 0, .hash = Hash.ZEROES },
+    );
+    defer fc.deinit();
+
+    _ = try fc.markForkInvalidCandidate(
         &SlotHashKey{ .slot = 0, .hash = Hash.ZEROES },
     );
 }
