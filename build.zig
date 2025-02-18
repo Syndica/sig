4f6205fa8907029f4137cbcdd1ee800f20399f4a
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2025-02-18 18:06:35 +0100

    Use in-house CLI library and remove global vars also remove zig-cli import from sig/cmd.zig

--- build.zig
+++ build.zig
@@ -119,7 +119,6 @@ pub fn build(b: *Build) !void {
 
     sig_mod.addImport("zig-network", zig_network_mod);
     sig_mod.addImport("base58", base58_mod);
-    sig_mod.addImport("zig-cli", zig_cli_mod);
     sig_mod.addImport("zstd", zstd_mod);
     sig_mod.addImport("poseidon", poseidon_mod);
 
@@ -149,7 +148,6 @@ pub fn build(b: *Build) !void {
 
     sig_exe.root_module.addImport("xev", xev_mod);
     sig_exe.root_module.addImport("base58", base58_mod);
-    sig_exe.root_module.addImport("zig-cli", zig_cli_mod);
     sig_exe.root_module.addImport("zig-network", zig_network_mod);
     sig_exe.root_module.addImport("zstd", zstd_mod);
     sig_exe.root_module.addImport("lsquic", lsquic_mod);
