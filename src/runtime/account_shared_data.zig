1996fabfedbf19db8fe1128e1dfee28da1a4405d
Harold Newman <hnewman2744@gmail.com>
2025-02-19 04:59:31 +1100

    Address PR comments

--- src/runtime/account_shared_data.zig
+++ src/runtime/account_shared_data.zig
@@ -7,7 +7,7 @@ const Epoch = sig.core.Epoch;
 /// `AccountSharedData` holds account information with a shared reference to the account data field
 /// `AccountSharedData`'s are loaded from `accounts_db` during the transaction loading phase
 ///
-/// TODO: move to `accounts_db`?
+/// TODO: move to `accounts_db` after implementing account loading?
 ///
 /// [agave] https://github.com/anza-xyz/agave/blob/faea52f338df8521864ab7ce97b120b2abb5ce13/sdk/src/account.rs#L118
 pub const AccountSharedData = struct {
@@ -23,18 +23,16 @@ pub const AccountSharedData = struct {
     rent_epoch: Epoch,
 
     pub fn isZeroed(self: AccountSharedData) bool {
-        // TODO: naive implementation
-        for (self.data) |byte| if (byte != 0) return false;
-        return true;
+        return std.mem.allEqual(u8, self.data, 0);
     }
 
+    /// Copy the old data into the new memory
+    /// If the new size is less than the old size, truncate the data
     pub fn resize(self: *AccountSharedData, allocator: std.mem.Allocator, new_size: usize) !void {
-        // TODO: naive implementation
         const new_memory = try allocator.alloc(u8, new_size);
         @memset(new_memory, 0);
-        @memcpy(new_memory[0..self.data.len], self.data);
+        @memcpy(new_memory.ptr, self.data[0..@min(self.data.len, new_size)]);
         allocator.free(self.data);
-        self.data.ptr = new_memory.ptr;
-        self.data.len = new_size;
+        self.data = new_memory;
     }
 };
