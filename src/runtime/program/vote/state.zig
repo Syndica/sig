f4291c6e8774be1df7e2c1ddfb1349788757615e
Dade <272535+dadepo@users.noreply.github.com>
2025-03-02 12:15:11 +0000

    Added versioned states

--- src/runtime/program/vote/state.zig
+++ src/runtime/program/vote/state.zig
@@ -51,11 +51,188 @@ pub const EpochCredit = struct {
     prev_credits: u64,
 };
 
+pub const VoteStateVersions = union(enum) {
+    v0_23_5: VoteState0_23_5,
+    v1_14_11: VoteState1_14_11,
+    current: VoteState,
+
+    fn landedVotesFromLockouts(allocator: std.mem.Allocator, lockouts: std.ArrayList(Lockout)) std.ArrayList(LandedVote) {
+        var landedVotes = std.ArrayList(LandedVote).init(allocator);
+        errdefer landedVotes.deinit();
+
+        for (lockouts.items) |lockout| {
+            try landedVotes.append(LandedVote{
+                .latency = 0,
+                .lockout = lockout,
+            });
+        }
+
+        return landedVotes;
+    }
+
+    pub fn convertToCurrent(self: VoteStateVersions, allocator: std.mem.Allocator) VoteState {
+        switch (self) {
+            .v0_23_5 => |state| return VoteState{
+                .node_pubkey = state.node_pubkey,
+                .authorized_withdrawer = state.authorized_withdrawer,
+                .commission = state.commission,
+                .votes = VoteStateVersions.landedVotesFromLockouts(allocator, state.votes),
+                .root_slot = state.root_slot,
+                .authorized_voter = state.authorized_voter,
+                .prior_voters = RingBuffer(PriorVote, MAX_PRIOR_VOTERS).DEFAULT,
+                .epoch_credits = state.epoch_credits,
+                .last_timestamp = state.last_timestamp,
+            },
+            .v1_14_11 => |state| return VoteState{
+                .node_pubkey = state.node_pubkey,
+                .authorized_withdrawer = state.authorized_withdrawer,
+                .commission = state.commission,
+                .votes = VoteStateVersions.landedVotesFromLockouts(allocator, state.votes),
+                .root_slot = state.root_slot,
+                .authorized_voter = state.authorized_voter,
+                .prior_voters = state.prior_voters,
+                .epoch_credits = state.epoch_credits,
+                .last_timestamp = state.last_timestamp,
+            },
+            .current => |state| return state,
+        }
+    }
+};
+
+pub const VoteState0_23_5 = struct {
+    /// the node that votes in this account
+    node_pubkey: Pubkey,
+
+    /// the signer for vote transactions
+    authorized_voter: Pubkey,
+    /// when the authorized voter was set/initialized
+    authorized_voter_epoch: Epoch,
+
+    /// history of prior authorized voters and the epoch ranges for which
+    ///  they were set
+    prior_voters: RingBuffer(PriorVote, MAX_PRIOR_VOTERS),
+
+    /// the signer for withdrawals
+    authorized_withdrawer: Pubkey,
+    /// percentage (0-100) that represents what part of a rewards
+    ///  payout should be given to this VoteAccount
+    commission: u8,
+
+    // TODO this should be a double ended queue.
+    votes: std.ArrayList(LandedVote),
+
+    root_slot: ?Slot,
+
+    /// history of how many credits earned by the end of each epoch
+    ///  each tuple is (Epoch, credits, prev_credits)
+    epoch_credits: std.ArrayList(EpochCredit),
+
+    /// most recent timestamp submitted with a vote
+    last_timestamp: BlockTimestamp,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        node_pubkey: Pubkey,
+        authorized_voter: Pubkey,
+        authorized_withdrawer: Pubkey,
+        commission: u8,
+        clock: Clock,
+    ) !VoteState0_23_5 {
+        return .{
+            .node_pubkey = node_pubkey,
+            .authorized_voter = authorized_voter,
+            .authorized_voter_epoch = clock.epoch,
+            .prior_voters = RingBuffer(PriorVote, MAX_PRIOR_VOTERS).DEFAULT,
+            .authorized_withdrawer = authorized_withdrawer,
+            .commission = commission,
+            .votes = std.ArrayList(LandedVote).init(allocator),
+            .root_slot = null,
+            .epoch_credits = std.ArrayList(EpochCredit).init(allocator),
+            .last_timestamp = BlockTimestamp{ .slot = 0, .timestamp = 0 },
+        };
+    }
+};
+
+pub const VoteState1_14_11 = struct {
+    /// the node that votes in this account
+    node_pubkey: Pubkey,
+
+    /// the signer for withdrawals
+    authorized_withdrawer: Pubkey,
+    /// percentage (0-100) that represents what part of a rewards
+    ///  payout should be given to this VoteAccount
+    commission: u8,
+
+    // TODO this should be a double ended queue.
+    votes: std.ArrayList(LandedVote),
+
+    // This usually the last Lockout which was popped from self.votes.
+    // However, it can be arbitrary slot, when being used inside Tower
+    root_slot: ?Slot,
+
+    /// the signer for vote transactions
+    authorized_voters: SortedMap(Epoch, Pubkey),
+
+    /// history of prior authorized voters and the epochs for which
+    /// they were set, the bottom end of the range is inclusive,
+    /// the top of the range is exclusive
+    prior_voters: RingBuffer(PriorVote, MAX_PRIOR_VOTERS),
+
+    /// history of how many credits earned by the end of each epoch
+    ///  each tuple is (Epoch, credits, prev_credits)
+    epoch_credits: std.ArrayList(EpochCredit),
+
+    /// most recent timestamp submitted with a vote
+    last_timestamp: BlockTimestamp,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        node_pubkey: Pubkey,
+        authorized_voter: Pubkey,
+        authorized_withdrawer: Pubkey,
+        commission: u8,
+        clock: Clock,
+    ) !VoteState1_14_11 {
+        var authorized_voters = SortedMap(Epoch, Pubkey).init(allocator);
+        errdefer authorized_voters.deinit();
+
+        authorized_voters.put(clock.epoch, authorized_voter) catch {
+            return InstructionError.Custom;
+        };
+
+        return .{
+            .node_pubkey = node_pubkey,
+            .authorized_withdrawer = authorized_withdrawer,
+            .commission = commission,
+            .votes = std.ArrayList(LandedVote).init(allocator),
+            .root_slot = null,
+            .authorized_voters = authorized_voters,
+            .prior_voters = RingBuffer(PriorVote, MAX_PRIOR_VOTERS).DEFAULT,
+            .epoch_credits = std.ArrayList(EpochCredit).init(allocator),
+            .last_timestamp = BlockTimestamp{ .slot = 0, .timestamp = 0 },
+        };
+    }
+
+    pub fn deinit(self: VoteState) void {
+        self.votes.deinit();
+        self.authorized_voters.deinit();
+        self.epoch_credits.deinit();
+    }
+
+    /// Upper limit on the size of the Vote State
+    /// when votes.len() is MAX_LOCKOUT_HISTORY.
+    pub fn sizeOf() usize {
+        return 3731;
+    }
+
+    pub fn serializedSize(self: VoteState) !usize {
+        return sig.bincode.sizeOf(self, .{});
+    }
+};
+
 /// [Agave] https://github.com/anza-xyz/solana-sdk/blob/991954602e718d646c0d28717e135314f72cdb78/vote-interface/src/state/mod.rs#L422
 ///
 /// Must support `bincode` and `serializedSize` methods for writing to the account data.
-///
-/// TODO Add versioned state for instructions that operates on existing vote accounts.
 pub const VoteState = struct {
     /// the node that votes in this account
     node_pubkey: Pubkey,
