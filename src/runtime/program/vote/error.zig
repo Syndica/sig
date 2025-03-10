bf35c2090f09a19d83c8afdaf5975b45bf1cb7b0
Dade <272535+dadepo@users.noreply.github.com>
2025-03-10 05:40:27 +0000

    Documentation and improvement to Authorize

--- src/runtime/program/vote/error.zig
+++ src/runtime/program/vote/error.zig
@@ -1,8 +1,7 @@
 const sig = @import("../../../sig.zig");
-const InstructionError = sig.core.instruction.InstructionError;
 
 /// Agave https://github.com/anza-xyz/solana-sdk/blob/4e30766b8d327f0191df6490e48d9ef521956495/vote-interface/src/error.rs#L11
-/// 
+///
 /// Reasons the vote might have had an error
 pub const VoteError = error{
     VoteTooOld,
