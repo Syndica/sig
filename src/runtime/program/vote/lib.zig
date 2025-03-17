ad089963449e86f1d055fa596cd7eb4f09377a18
Dade <272535+dadepo@users.noreply.github.com>
2025-03-17 19:52:02 +0000

    Fix entrypoint

--- src/runtime/program/vote/lib.zig
+++ src/runtime/program/vote/lib.zig
@@ -4,7 +4,7 @@ pub const state = @import("state.zig");
 
 /// https://github.com/solana-program/vote/blob/f6e499f2a29d890896af4f063d8eea762d4d43b7/program/src/lib.rs#L7C30-L7C73
 pub const ID =
-sig.core.Pubkey.parseBase58String(
+    sig.core.Pubkey.parseBase58String(
     "Vote111111111111111111111111111111111111111",
 ) catch unreachable;
 
@@ -14,4 +14,4 @@ pub const vote_instruction = @import("instruction.zig");
 
 pub const VoteError = @import("error.zig").VoteError;
 pub const VoteAuthorize = state.VoteAuthorize;
-pub const entrypoint = @import("execute.zig").entrypoint;
+pub const execute = @import("execute.zig").execute;
