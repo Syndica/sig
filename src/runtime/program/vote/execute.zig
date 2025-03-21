d205859c6ed412b4578374cfcd66b50e0fc0909b
Dade <272535+dadepo@users.noreply.github.com>
2025-03-21 20:25:33 +0000

    Agave permalinks

--- src/runtime/program/vote/execute.zig
+++ src/runtime/program/vote/execute.zig
@@ -217,10 +217,15 @@ fn authorize(
         .voter => {
             const current_epoch = clock.epoch;
 
-            const authorized_withdrawer_signer = !std.meta.isError(validateIsSigner(
-                vote_state.authorized_withdrawer,
-                signers,
-            ));
+            const authorized_withdrawer_signer = blk: {
+                validateIsSigner(
+                    vote_state.authorized_withdrawer,
+                    signers,
+                ) catch {
+                    break :blk false;
+                };
+                break :blk true;
+            };
 
             // [agave] https://github.com/anza-xyz/agave/blob/01e50dc39bde9a37a9f15d64069459fe7502ec3e/programs/vote/src/vote_state/mod.rs#L697-L701
             const target_epoch = std.math.add(u64, current_epoch, 1) catch {
