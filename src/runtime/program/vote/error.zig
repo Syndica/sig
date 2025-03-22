088dde878a1ec3133e68ca1b2a95921b12970a0d
Dade <272535+dadepo@users.noreply.github.com>
2025-03-22 05:58:36 +0000

    Fix style after rebase

--- src/runtime/program/vote/error.zig
+++ src/runtime/program/vote/error.zig
@@ -1,5 +1,3 @@
-const sig = @import("../../../sig.zig");
-
 /// [agave] https://github.com/anza-xyz/solana-sdk/blob/4e30766b8d327f0191df6490e48d9ef521956495/vote-interface/src/error.rs#L11
 ///
 /// Reasons the vote might have had an error
