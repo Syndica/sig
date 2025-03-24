5a8ed6d71c631e22b5a3cb285934871f97b92e90
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-03-24 20:17:07 +0400

    Some fmt

--- src/runtime/program/vote/execute.zig
+++ src/runtime/program/vote/execute.zig
@@ -87,8 +87,18 @@ pub fn execute(
             ic,
             &vote_account,
         ),
-        .update_commission => |args| executeUpdateCommission(allocator, ic, &vote_account, args),
-        .withdraw => |args| executeWithdraw(allocator, ic, &vote_account, args),
+        .update_commission => |args| executeUpdateCommission(
+            allocator,
+            ic,
+            &vote_account,
+            args,
+        ),
+        .withdraw => |args| executeWithdraw(
+            allocator,
+            ic,
+            &vote_account,
+            args,
+        ),
     };
 }
 
