eec472acdfbe8cff35b333553136247ccce2f9c8
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-03-24 16:49:32 +0400

    Fixes after rebase

--- src/runtime/program/vote/state.zig
+++ src/runtime/program/vote/state.zig
@@ -1,4 +1,4 @@
-/// [agave] Analogous to https://github.com/anza-xyz/solana-sdk/blob/991954602e718d646c0d28717e135314f72cdb78/vote-interface/src/state/mod.rs#L1
+/// Analogous to https://github.com/anza-xyz/solana-sdk/blob/991954602e718d646c0d28717e135314f72cdb78/vote-interface/src/state/mod.rs#L1
 const std = @import("std");
 const sig = @import("../../../sig.zig");
 const builtin = @import("builtin");
