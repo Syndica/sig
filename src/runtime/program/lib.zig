e6af63d8b851f63f329e95b8db79c4c328c71b06
Harold Newman <hnewman2744@gmail.com>
2025-03-26 07:51:08 +1100

    Implement basic bpf execution

--- src/runtime/program/lib.zig
+++ src/runtime/program/lib.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 const sig = @import("../../sig.zig");
 
-const InstructionError = sig.core.instruction.InstructionError;
-const InstructionContext = sig.runtime.InstructionContext;
-
 pub const address_lookup_table = @import("address_lookup_table/lib.zig");
 pub const bpf_loader_program = @import("bpf_loader/lib.zig");
 pub const precompile_programs = @import("precompiles/lib.zig");
@@ -11,6 +8,11 @@ pub const system_program = @import("system/lib.zig");
 pub const serialization = @import("serialization.zig");
 pub const testing = @import("testing.zig");
 pub const vote_program = @import("vote/lib.zig");
+
+pub const test_bpf_execution = @import("test_bpf_execution.zig");
+
+const InstructionError = sig.core.instruction.InstructionError;
+const InstructionContext = sig.runtime.InstructionContext;
 
 pub const PROGRAM_ENTRYPOINTS = initProgramEntrypoints();
 pub const PRECOMPILE_ENTRYPOINTS = initPrecompileEntrypoints();
