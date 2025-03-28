ef6ccc19e03ab070258f0201a617c20627772405
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-03-29 00:29:31 +0400

    Rebase fix

--- src/runtime/program/vote/state.zig
+++ src/runtime/program/vote/state.zig
@@ -582,13 +582,6 @@ pub const VoteState = struct {
         return pubkey;
     }
 
-    /// [agave] https://github.com/anza-xyz/agave/blob/9806724b6d49dec06a9d50396adf26565d6b7745/programs/vote/src/vote_state/mod.rs#L792
-    ///
-    /// Given a proposed new commission, returns true if this would be a commission increase, false otherwise
-    pub fn isCommissionIncrease(self: *const VoteState, commission: u8) bool {
-        return commission > self.commission;
-    }
-
     pub fn lastLockout(self: *const VoteState) ?Lockout {
         if (self.votes.getLastOrNull()) |vote| {
             return vote.lockout;
