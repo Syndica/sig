0ad5c06e78a8ef49a7b9c347a26e9dbe0f221c0d
kprotty <kbutcher6200@gmail.com>
2025-04-11 08:19:49 -0500

    fix AccountMetaC vm_addr in test

--- src/vm/syscalls/cpi.zig
+++ src/vm/syscalls/cpi.zig
@@ -1402,7 +1402,7 @@ fn testTranslateInstruction(comptime AccountInfoType: type) !void {
 
     const accounts_len = @sizeOf(AccountMetaType) * accounts.len;
     var total_size = @sizeOf(InstructionType) + accounts_len + data.len;
-    
+
     const keys_offset = total_size;
     if (AccountInfoType == AccountInfoC) {
         total_size += @sizeOf(Pubkey) + (accounts.len * @sizeOf(Pubkey));
@@ -1451,7 +1451,7 @@ fn testTranslateInstruction(comptime AccountInfoType: type) !void {
     for (accounts, 0..) |ins_account, i| {
         const account_meta: AccountMetaType = switch (AccountMetaType) {
             AccountMetaC => .{
-                .pubkey_addr = keys_offset + ((i + 1) * @sizeOf(Pubkey)),
+                .pubkey_addr = vm_addr + keys_offset + ((i + 1) * @sizeOf(Pubkey)),
                 .is_writable = ins_account.is_writable,
                 .is_signer = ins_account.is_signer,
             },
