2c997fffa4bc15c2c23d857fec960f66d28d5d0d
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2025-04-11 02:39:14 +0200

    Implement & test the `ProgressMap` data structure First pass which mirrors the rust implementation and has a unit test validating that the ownership semantics in and between all of the data structures are sound.

--- src/consensus/lib.zig
+++ src/consensus/lib.zig
@@ -1,5 +1,13 @@
 pub const fork_choice = @import("fork_choice.zig");
+pub const progress_map = @import("progress_map.zig");
+
+comptime {
+    _ = fork_choice;
+    _ = progress_map;
+}
 
 pub const HeaviestSubtreeForkChoice = fork_choice.ForkChoice;
 pub const ForkWeight = fork_choice.ForkWeight;
 pub const ForkInfo = fork_choice.ForkInfo;
+
+pub const ProgressMap = progress_map.ProgressMap;
