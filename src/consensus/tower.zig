36c2274495c9fc8eed24f938508d4ab0f2253582
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-04-15 15:26:02 +0400

    Move unimplemented or partially implemented dependency to unimplemented.zig

--- src/consensus/tower.zig
+++ src/consensus/tower.zig
@@ -9,6 +9,15 @@ const AutoHashMap = std.AutoHashMap;
 const BankForks = sig.consensus.unimplemented.BankForks;
 const ReplayStage = sig.consensus.unimplemented.ReplayStage;
 const ProgressMap = sig.consensus.unimplemented.ProgressMap;
+const VoteTransaction = sig.consensus.unimplemented.VoteTransaction;
+const Check = sig.consensus.unimplemented.Check;
+const SlotHistory = sig.consensus.unimplemented.SlotHistory;
+const LatestValidatorVotesForFrozenBanks =
+    sig.consensus.unimplemented.LatestValidatorVotesForFrozenBanks;
+const VoteAccountsHashMap = sig.consensus.unimplemented.VoteAccountsHashMap;
+const LockoutIntervals = sig.consensus.unimplemented.LockoutIntervals;
+const VotedSlotAndPubkey = sig.consensus.unimplemented.VotedSlotAndPubkey;
+const TowerVoteState = sig.consensus.unimplemented.TowerVoteState;
 
 const TowerStorage = sig.consensus.tower_storage.TowerStorage;
 const SavedTower = sig.consensus.tower_storage.SavedTower;
@@ -19,7 +28,6 @@ const Vote = sig.runtime.program.vote_program.state.Vote;
 const VoteStateVersions = sig.runtime.program.vote_program.state.VoteStateVersions;
 const VoteState = sig.runtime.program.vote_program.state.VoteState;
 const VoteError = sig.runtime.program.vote_program.VoteError;
-const MAX_LOCKOUT_HISTORY = sig.runtime.program.vote_program.state.MAX_LOCKOUT_HISTORY;
 const HeaviestSubtreeForkChoice = sig.consensus.HeaviestSubtreeForkChoice;
 const VoteStateUpdate = sig.runtime.program.vote_program.state.VoteStateUpdate;
 const TowerSync = sig.runtime.program.vote_program.state.TowerSync;
@@ -35,190 +43,13 @@ const Account = sig.core.Account;
 
 const UnixTimestamp = i64;
 
+const MAX_LOCKOUT_HISTORY = sig.runtime.program.vote_program.state.MAX_LOCKOUT_HISTORY;
 const VOTE_THRESHOLD_DEPTH_SHALLOW: usize = 4;
 pub const VOTE_THRESHOLD_DEPTH: usize = 8;
 pub const SWITCH_FORK_THRESHOLD: f64 = 0.38;
 pub const DUPLICATE_LIVENESS_THRESHOLD: f64 = 0.1;
-
-// TODO DUPLICATE_THRESHOLD is defined in replay stage in Agave
-pub const DUPLICATE_THRESHOLD: f64 = 1.0 - SWITCH_FORK_THRESHOLD - DUPLICATE_LIVENESS_THRESHOLD;
-
-// TODO this is defined in a different crate in Agave
-pub const VoteTransaction = union(enum) {
-    vote: Vote,
-    vote_state_update: VoteStateUpdate,
-    // TODO Check the serialisation for the difference with compact_
-    compact_vote_state_update: VoteStateUpdate,
-    tower_sync: TowerSync,
-
-    pub fn default(allocator: std.mem.Allocator) !VoteTransaction {
-        return VoteTransaction{ .tower_sync = try TowerSync.default(allocator) };
-    }
-
-    pub fn timestamp(self: *const VoteTransaction) ?UnixTimestamp {
-        return switch (self.*) {
-            .vote => |args| args.timestamp,
-            .vote_state_update => |args| args.timestamp,
-            .compact_vote_state_update => |args| args.timestamp,
-            .tower_sync => |args| args.timestamp,
-        };
-    }
-
-    pub fn lastVotedSlot(self: *const VoteTransaction) ?Slot {
-        return switch (self.*) {
-            .vote => |args| args.slots[args.slots.len - 1],
-            .vote_state_update => |args| args.lockouts.items[args.lockouts.items.len - 1].slot,
-            .compact_vote_state_update => |args| args.lockouts.items[
-                args.lockouts.items.len - 1
-            ].slot,
-            .tower_sync => |args| args.lockouts.items[args.lockouts.items.len - 1].slot,
-        };
-    }
-
-    pub fn setTimestamp(self: *VoteTransaction, ts: ?UnixTimestamp) void {
-        switch (self.*) {
-            .vote => |*vote| vote.timestamp = ts,
-            .vote_state_update, .compact_vote_state_update => |*vote_state_update| {
-                vote_state_update.timestamp = ts;
-            },
-            .tower_sync => |*tower_sync| tower_sync.timestamp = ts,
-        }
-    }
-
-    pub fn isEmpty(self: *const VoteTransaction) bool {
-        return switch (self.*) {
-            .vote => |vote| vote.slots.len == 0,
-            .vote_state_update, .compact_vote_state_update => |vote_state_update| vote_state_update
-                .lockouts.items.len == 0,
-            .tower_sync => |tower_sync| tower_sync.lockouts.items.len == 0,
-        };
-    }
-
-    pub fn slot(self: *const VoteTransaction, i: usize) Slot {
-        return switch (self.*) {
-            .vote => |vote| vote.slots[i],
-            .vote_state_update, .compact_vote_state_update => |vote_state_update| vote_state_update
-                .lockouts.items[i].slot,
-            .tower_sync => |tower_sync| tower_sync.lockouts.items[i].slot,
-        };
-    }
-
-    pub fn len(self: *const VoteTransaction) usize {
-        return switch (self.*) {
-            .vote => |vote| vote.slots.len,
-            .vote_state_update, .compact_vote_state_update => |vote_state_update| vote_state_update
-                .lockouts.items.len,
-            .tower_sync => |tower_sync| tower_sync.lockouts.items.len,
-        };
-    }
-
-    pub fn hash(self: *const VoteTransaction) Hash {
-        return switch (self.*) {
-            .vote => |vote| vote.hash,
-            .vote_state_update, .compact_vote_state_update => |vote_state_update| vote_state_update
-                .hash,
-            .tower_sync => |tower_sync| tower_sync.hash,
-        };
-    }
-
-    pub fn eql(self: *const VoteTransaction, other: *const VoteTransaction) bool {
-        // TODO implement
-        _ = self;
-        _ = other;
-        return true;
-    }
-};
-
 pub const MAX_ENTRIES: u64 = 1024 * 1024; // 1 million slots is about 5 days
-
-// TODO this is defined in a different crate in Agave
-//
-pub const Check = enum {
-    future,
-    too_old,
-    found,
-    not_found,
-};
-
-pub const SlotHistory = struct {
-    bits: std.DynamicBitSet,
-    next_slot: u64,
-
-    pub fn deinit(self: *SlotHistory) void {
-        self.bits.deinit();
-    }
-    pub fn add(self: *SlotHistory, slot: u64) void {
-        if (slot > self.next_slot and
-            slot - self.next_slot >= MAX_ENTRIES)
-        {
-            // Wrapped past current history,
-            // clear entire bitvec.
-            const full_blocks = @as(usize, MAX_ENTRIES) / 64;
-            for (0..full_blocks) |i| {
-                self.bits.unset(i);
-            }
-        } else {
-            for (self.next_slot..slot) |skipped| {
-                self.bits.unset(skipped % MAX_ENTRIES);
-            }
-        }
-
-        self.bits.set(slot % MAX_ENTRIES);
-        self.next_slot = slot + 1;
-    }
-
-    pub fn check(self: *const SlotHistory, slot: u64) Check {
-        if (slot > self.newest()) {
-            return Check.future;
-        } else if (slot < self.oldest()) {
-            return Check.too_old;
-        } else if (self.bits.isSet(slot % MAX_ENTRIES)) {
-            return Check.found;
-        } else {
-            return Check.not_found;
-        }
-    }
-
-    pub fn oldest(self: *const SlotHistory) u64 {
-        return self.next_slot -| MAX_ENTRIES;
-    }
-
-    pub fn newest(self: *const SlotHistory) u64 {
-        return self.next_slot - 1;
-    }
-
-    pub fn clone(
-        self: *const SlotHistory,
-        allocator: std.mem.Allocator,
-    ) !SlotHistory {
-        return SlotHistory{
-            .bits = try self.bits.clone(allocator),
-            .next_slot = self.next_slot,
-        };
-    }
-};
-
-// TODO Needs to be implemented and moved out of the tower.zig
-pub const LatestValidatorVotesForFrozenBanks = struct {
-    max_gossip_frozen_votes: std.AutoHashMap(Pubkey, struct { slot: Slot, hashes: []Hash }),
-    pub fn checkAndVote(
-        self: *LatestValidatorVotesForFrozenBanks,
-        vote_pubkey: Pubkey,
-        frozend_hash: ?Hash,
-        is_replay_vote: bool,
-    ) struct { bool, ?Slot } {
-        _ = self;
-        _ = vote_pubkey;
-        _ = frozend_hash;
-        _ = is_replay_vote;
-        @panic("Unimplemented");
-    }
-};
-pub const VoteAccount = struct {};
-
-pub const StakedAccount = struct { stake: u64, account: VoteAccount };
-
-pub const VoteAccountsHashMap = std.AutoHashMap(Pubkey, StakedAccount);
+pub const DUPLICATE_THRESHOLD = sig.consensus.unimplemented.DUPLICATE_THRESHOLD;
 
 const SwitchForkDecision = union(enum) {
     switch_proof: Hash,
@@ -232,14 +63,10 @@ const SwitchForkDecision = union(enum) {
     failed_switch_duplicate_rollback: Slot,
 };
 
-const VotedSlotAndPubkey = struct { slot: Slot, pubkey: Pubkey };
-
 pub const Stake = u64;
-pub const ExpirationSlot = Slot;
+
 pub const VotedSlot = Slot;
 pub const VotedStakes = AutoHashMap(Slot, Stake);
-// TODO modify SortedMap to allow array in value - support eq
-pub const LockoutIntervals = SortedMap(ExpirationSlot, std.ArrayList(VotedSlotAndPubkey));
 
 const ComputedBankState = struct {
     voted_stakes: VotedStakes,
@@ -259,97 +86,6 @@ pub const ThresholdDecision = union(enum) {
         // Observed stake
         u64,
     },
-};
-
-const TowerVoteState = struct {
-    // TODO confirm if this can be a slice
-    votes: std.ArrayListUnmanaged(Lockout),
-    root_slot: ?Slot,
-
-    pub fn default(allocator: std.mem.Allocator) !TowerVoteState {
-        return .{
-            .votes = try std.ArrayListUnmanaged(Lockout).initCapacity(allocator, 0),
-            .root_slot = null,
-        };
-    }
-
-    pub fn lastLockout(self: *const TowerVoteState) ?*const Lockout {
-        if (self.votes.items.len == 0) return null;
-        return &self.votes.items[self.votes.items.len - 1];
-    }
-
-    pub fn lastVotedSlot(self: *const TowerVoteState) ?Slot {
-        if (self.lastLockout() == null) return null;
-        return self.lastLockout().?.slot;
-    }
-
-    pub fn clone(
-        self: TowerVoteState,
-        allocator: std.mem.Allocator,
-    ) (error{OutOfMemory})!TowerVoteState {
-        return .{ .votes = try self.votes.clone(allocator), .root_slot = self.root_slot };
-    }
-
-    pub fn nthRecentLockout(self: *const TowerVoteState, position: usize) ?*const Lockout {
-        const pos = std.math.add(usize, self.votes.items.len, position +| 1) catch
-            return null;
-        return &self.votes.items[pos];
-    }
-
-    pub fn processNextVoteSlot(
-        self: *TowerVoteState,
-        allocator: std.mem.Allocator,
-        next_vote_slot: Slot,
-    ) !void {
-        // Ignore votes for slots earlier than we already have votes for
-        if (self.lastVotedSlot()) |last_voted_slot| {
-            if (next_vote_slot <= last_voted_slot) {
-                return;
-            }
-        }
-
-        self.popExpiredVotes(next_vote_slot);
-
-        // Once the stack is full, pop the oldest lockout and distribute rewards
-        if (self.votes.items.len == MAX_LOCKOUT_HISTORY) {
-            const rooted_vote = self.votes.orderedRemove(0);
-            self.root_slot = rooted_vote.slot;
-        }
-        try self.votes.append(
-            allocator,
-            Lockout{ .slot = next_vote_slot, .confirmation_count = 1 },
-        );
-        try self.doubleLockouts();
-    }
-
-    // Pop all recent votes that are not locked out at the next vote slot.  This
-    // allows validators to switch forks once their votes for another fork have
-    // expired. This also allows validators continue voting on recent blocks in
-    // the same fork without increasing lockouts.
-    pub fn popExpiredVotes(self: *TowerVoteState, next_vote_slot: Slot) void {
-        while (self.lastLockout()) |vote| {
-            if (!vote.isLockedOutAtSlot(next_vote_slot)) {
-                _ = self.votes.popOrNull();
-            } else {
-                break;
-            }
-        }
-    }
-
-    pub fn doubleLockouts(self: *TowerVoteState) !void {
-        const stack_depth = self.votes.items.len;
-
-        for (self.votes.items, 0..) |*vote, i| {
-            // Don't increase the lockout for this vote until we get more confirmations
-            // than the max number of confirmations this vote has seen
-            const confirmation_count = vote.confirmation_count;
-            if (stack_depth > std.math.add(usize, i, confirmation_count) catch
-                return error.ArithmeticOverflow)
-            {
-                vote.confirmation_count +|= 1;
-            }
-        }
-    }
 };
 
 const BlockhashStatus = union(enum) {
