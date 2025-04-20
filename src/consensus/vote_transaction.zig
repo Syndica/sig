07d3855549d3f27a165145630ddcf3df41a8633f
Dade <272535+dadepo@users.noreply.github.com>
2025-04-20 06:24:12 +0000

    Increasing test coverage for vote_transaction

--- src/consensus/vote_transaction.zig
+++ src/consensus/vote_transaction.zig
@@ -240,14 +240,74 @@ test "vote_transaction.VoteTransaction - variant equality" {
         .{ .slot = 2, .confirmation_count = 2 },
     );
 
-    try std.testing.expect(visual_state_update1.eql(&visual_state_update2));
+    // Test vote_state_update equality
+    var compact_visual_state_update1 = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout).initCapacity(std.testing.allocator, 3),
+        .hash = Hash.ZEROES,
+        .timestamp = 200,
+        .root = 1,
+    } };
+    defer compact_visual_state_update1.deinit(std.testing.allocator);
+    compact_visual_state_update1.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    compact_visual_state_update1.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+
+    var compact_visual_state_update2 = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout).initCapacity(std.testing.allocator, 3),
+        .hash = Hash.ZEROES,
+        .timestamp = 200,
+        .root = 1,
+    } };
+    defer compact_visual_state_update2.deinit(std.testing.allocator);
+    compact_visual_state_update2.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    compact_visual_state_update2.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+
+    // Test tower_sync equality
+    var tower_sync1 = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout).initCapacity(std.testing.allocator, 3),
+        .hash = Hash.ZEROES,
+        .timestamp = 200,
+        .root = 1,
+        .block_id = Hash.ZEROES,
+    } };
+    defer tower_sync1.deinit(std.testing.allocator);
+    tower_sync1.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    tower_sync1.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+
+    var tower_sync2 = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout).initCapacity(std.testing.allocator, 3),
+        .hash = Hash.ZEROES,
+        .timestamp = 200,
+        .root = 1,
+        .block_id = Hash.ZEROES,
+    } };
+    defer tower_sync2.deinit(std.testing.allocator);
+    tower_sync2.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    tower_sync2.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+
+    try std.testing.expect(compact_visual_state_update1.eql(&compact_visual_state_update2));
 
     // Test different variant inequality
     try std.testing.expect(!vote1.eql(&visual_state_update1));
 }
 
 test "vote_transaction.VoteTransaction - timestamp operations" {
-    const vote = VoteTransaction{ .vote = .{
+    var vote = VoteTransaction{ .vote = .{
         .slots = &[_]Slot{1},
         .hash = Hash.ZEROES,
         .timestamp = null,
@@ -255,9 +315,60 @@ test "vote_transaction.VoteTransaction - timestamp operations" {
 
     try std.testing.expectEqual(@as(?UnixTimestamp, null), vote.timestamp());
 
-    var mut_vote = vote;
-    mut_vote.setTimestamp(100);
-    try std.testing.expectEqual(@as(?UnixTimestamp, 100), mut_vote.timestamp());
+    vote.setTimestamp(100);
+    try std.testing.expectEqual(@as(?UnixTimestamp, 100), vote.timestamp());
+
+    var vote_state_updated = VoteTransaction{ .vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 0),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+
+    try std.testing.expectEqual(@as(?UnixTimestamp, null), vote_state_updated.timestamp());
+
+    vote_state_updated.setTimestamp(100);
+    try std.testing.expectEqual(@as(?UnixTimestamp, 100), vote_state_updated.timestamp());
+
+    var compact_vote_state_updated = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 0),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+
+    try std.testing.expectEqual(
+        @as(?UnixTimestamp, null),
+        compact_vote_state_updated.timestamp(),
+    );
+
+    compact_vote_state_updated.setTimestamp(100);
+    try std.testing.expectEqual(
+        @as(?UnixTimestamp, 100),
+        compact_vote_state_updated.timestamp(),
+    );
+
+    var tower_sync = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 0),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+        .block_id = Hash.ZEROES,
+    } };
+
+    try std.testing.expectEqual(
+        @as(?UnixTimestamp, null),
+        tower_sync.timestamp(),
+    );
+
+    tower_sync.setTimestamp(100);
+    try std.testing.expectEqual(
+        @as(?UnixTimestamp, 100),
+        tower_sync.timestamp(),
+    );
 }
 
 test "vote_transaction.VoteTransaction - lastVotedSlot" {
@@ -290,6 +401,39 @@ test "vote_transaction.VoteTransaction - lastVotedSlot" {
         .{ .slot = 20, .confirmation_count = 2 },
     );
     try std.testing.expectEqual(20, vote_state_update.lastVotedSlot());
+
+    var compact_vote_state_update = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer compact_vote_state_update.deinit(std.testing.allocator);
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 10, .confirmation_count = 1 },
+    );
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 20, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(20, compact_vote_state_update.lastVotedSlot());
+
+    var towe_sync = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+        .block_id = Hash.ZEROES,
+    } };
+    defer towe_sync.deinit(std.testing.allocator);
+    towe_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 10, .confirmation_count = 1 },
+    );
+    towe_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 20, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(20, towe_sync.lastVotedSlot());
 }
 
 test "vote_transaction.VoteTransaction - slot access" {
@@ -301,6 +445,58 @@ test "vote_transaction.VoteTransaction - slot access" {
     try std.testing.expectEqual(10, vote.slot(0));
     try std.testing.expectEqual(20, vote.slot(1));
     try std.testing.expectEqual(30, vote.slot(2));
+
+    var vote_state_update = VoteTransaction{ .vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer vote_state_update.deinit(std.testing.allocator);
+    vote_state_update.vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 10, .confirmation_count = 1 },
+    );
+    vote_state_update.vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 20, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(10, vote_state_update.slot(0));
+    try std.testing.expectEqual(20, vote_state_update.slot(1));
+
+    var compact_vote_state_update = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer compact_vote_state_update.deinit(std.testing.allocator);
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 10, .confirmation_count = 1 },
+    );
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 20, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(10, compact_vote_state_update.slot(0));
+    try std.testing.expectEqual(20, compact_vote_state_update.slot(1));
+
+    var tower_sync = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+        .block_id = Hash.ZEROES,
+    } };
+    defer tower_sync.deinit(std.testing.allocator);
+    tower_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 10, .confirmation_count = 1 },
+    );
+    tower_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 20, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(10, tower_sync.slot(0));
+    try std.testing.expectEqual(20, tower_sync.slot(1));
 }
 
 test "vote_transaction.VoteTransaction - length" {
@@ -327,6 +523,41 @@ test "vote_transaction.VoteTransaction - length" {
         .{ .slot = 2, .confirmation_count = 2 },
     );
     try std.testing.expectEqual(2, vote_state_update.len());
+
+    var compact_vote_state_update = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer compact_vote_state_update.deinit(std.testing.allocator);
+
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    compact_vote_state_update.compact_vote_state_update.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(2, compact_vote_state_update.len());
+
+    var tower_sync = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = Hash.ZEROES,
+        .timestamp = null,
+        .root = 100,
+        .block_id = Hash.ZEROES,
+    } };
+    defer tower_sync.deinit(std.testing.allocator);
+
+    tower_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 1, .confirmation_count = 1 },
+    );
+    tower_sync.tower_sync.lockouts.appendAssumeCapacity(
+        .{ .slot = 2, .confirmation_count = 2 },
+    );
+    try std.testing.expectEqual(2, tower_sync.len());
 }
 
 test "vote_transaction.VoteTransaction - hash" {
@@ -337,4 +568,35 @@ test "vote_transaction.VoteTransaction - hash" {
         .timestamp = null,
     } };
     try std.testing.expect(test_hash.eql(vote.hash()));
+
+    var vote_state_update = VoteTransaction{ .vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = test_hash,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer vote_state_update.deinit(std.testing.allocator);
+    try std.testing.expect(test_hash.eql(vote_state_update.hash()));
+
+    var compact_vote_state_update = VoteTransaction{ .compact_vote_state_update = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = test_hash,
+        .timestamp = null,
+        .root = 100,
+    } };
+    defer compact_vote_state_update.deinit(std.testing.allocator);
+    try std.testing.expect(test_hash.eql(compact_vote_state_update.hash()));
+
+    var tower_sync = VoteTransaction{ .tower_sync = .{
+        .lockouts = try std.ArrayListUnmanaged(Lockout)
+            .initCapacity(std.testing.allocator, 2),
+        .hash = test_hash,
+        .timestamp = null,
+        .root = 100,
+        .block_id = Hash.ZEROES,
+    } };
+    defer tower_sync.deinit(std.testing.allocator);
+    try std.testing.expect(test_hash.eql(tower_sync.hash()));
 }
