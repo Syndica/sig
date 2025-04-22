a7977ef962e93a167af9e213634c066acb55dba7
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2025-04-22 20:38:02 +0200

    Remove pointless comptime block reference

--- src/consensus/lib.zig
+++ src/consensus/lib.zig
@@ -1,11 +1,6 @@
 pub const fork_choice = @import("fork_choice.zig");
 pub const progress_map = @import("progress_map.zig");
 
-comptime {
-    _ = fork_choice;
-    _ = progress_map;
-}
-
 pub const HeaviestSubtreeForkChoice = fork_choice.ForkChoice;
 pub const ForkWeight = fork_choice.ForkWeight;
 pub const ForkInfo = fork_choice.ForkInfo;
