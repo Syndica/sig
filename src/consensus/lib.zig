df53d447a71fc6334b071161d232107af5da4033
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2025-04-28 22:36:27 +0200

    VoteTracker MVP

--- src/consensus/lib.zig
+++ src/consensus/lib.zig
@@ -1,5 +1,8 @@
 pub const fork_choice = @import("fork_choice.zig");
+pub const vote_tracker = @import("vote_tracker.zig");
 
 pub const HeaviestSubtreeForkChoice = fork_choice.ForkChoice;
 pub const ForkWeight = fork_choice.ForkWeight;
 pub const ForkInfo = fork_choice.ForkInfo;
+
+pub const VoteTracker = vote_tracker.VoteTracker;
