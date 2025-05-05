6b99939bfb5ddff2b36b3cd2f2f2db3d5bd33ebc
kprotty <kbutcher6200@gmail.com>
2025-05-04 23:11:00 -0500

    test getSysvar StakeHistory

--- src/runtime/sysvar/stake_history.zig
+++ src/runtime/sysvar/stake_history.zig
@@ -11,6 +11,16 @@ pub const StakeHistory = struct {
     pub const ID =
         Pubkey.parseBase58String("SysvarStakeHistory1111111111111111111111111") catch unreachable;
 
+    pub const DEFAULT = StakeHistory{
+        .entries = &.{},
+    };
+
+    /// [stake] https://github.com/solana-program/stake/blob/bcec951fda5f2a30b1f4a058706d2e9ed23a8429/interface/src/stake_history.rs#L8
+    pub const MAX_ENTRIES = 512;
+
+    /// [agave] https://github.com/anza-xyz/solana-sdk/blob/ac11e3e568952977e63bce6bb20e37f26a61e151/sysvar/src/stake_history.rs#L66
+    pub const SIZE_OF = 16_392;
+
     pub const Entry = struct {
         Epoch,
         struct {
