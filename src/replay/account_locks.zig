01e889e2795ddd75e246cacf27affdfcac61eaf1
Drew Nutter <dnut@users.noreply.github.com>
2025-05-08 14:53:55 -0400

    feat(replay): implement account locks

---
+++ src/replay/account_locks.zig
@@ -0,0 +1,123 @@
+const std = @import("std");
+const sig = @import("../sig.zig");
+
+const Allocator = std.mem.Allocator;
+
+const Pubkey = sig.core.Pubkey;
+
+pub const AccountLocks = struct {
+    write_locks: std.AutoArrayHashMapUnmanaged(Pubkey, u64) = .{},
+    readonly_locks: std.AutoArrayHashMapUnmanaged(Pubkey, u64) = .{},
+
+    const LockError = Allocator.Error || error{LockFailed};
+
+    /// Either locks all accounts, or locks none and returns an error.
+    ///
+    /// This function does not allow there to be any inner conflicts. In other
+    /// words, if the passed batch has a single write lock, then any other
+    /// attempts to lock the same account within this batch will result in
+    /// failure.
+    pub fn lockStrict(
+        self: *AccountLocks,
+        allocator: Allocator,
+        /// { account to lock, write access needed }
+        accounts: []const struct { Pubkey, bool },
+    ) LockError!void {
+        for (accounts, 0..) |account, i| {
+            errdefer std.debug.assert(0 == self.unlock(accounts[0..i]));
+            const address, const write = account;
+            if (write) {
+                if (self.readonly_locks.contains(address)) {
+                    return error.LockFailed;
+                }
+                const entry = try self.write_locks.getOrPut(allocator, address);
+                if (entry.found_existing) {
+                    return error.LockFailed;
+                }
+                entry.value_ptr.* += 1;
+            } else {
+                if (self.write_locks.contains(address)) {
+                    return error.LockFailed;
+                }
+                const entry = try self.readonly_locks.getOrPut(allocator, address);
+                entry.value_ptr.* += 1;
+            }
+        }
+    }
+
+    /// Either locks all accounts, or locks none and returns an error.
+    ///
+    /// This function allows there to be inner conflicts within the passed
+    /// batch, as long as it doesn't conflict with prior lock calls. In other
+    /// words, the passed batch may succeed to lock with multiple write locks on
+    /// the same account, as long as there were no pre-existing locks on that
+    /// account before this function call.
+    pub fn lockPermissive(
+        self: *AccountLocks,
+        allocator: Allocator,
+        /// { account to lock, write access needed }
+        accounts: []const struct { Pubkey, bool },
+    ) LockError!void {
+        for (accounts) |account| {
+            const address, const write = account;
+            if (write) {
+                if (self.readonly_locks.contains(address) or self.write_locks.contains(address)) {
+                    return error.LockFailed;
+                }
+            } else if (self.write_locks.contains(address)) {
+                return error.LockFailed;
+            }
+        }
+        for (accounts, 0..) |account, i| {
+            errdefer std.debug.assert(0 == self.unlock(accounts[0..i]));
+            const address, const write = account;
+            if (write) {
+                _ = try self.write_locks.getOrPut(allocator, address);
+            } else {
+                const entry = try self.readonly_locks.getOrPut(allocator, address);
+                entry.value_ptr.* += 1;
+            }
+        }
+    }
+
+    /// Infallible function that guarantees all the provided accounts will be
+    /// unlocked after it returns.
+    ///
+    /// Returns the number of items that were already unlocked and thus did not
+    /// need to be unlocked. You can use this in a calling scope to assert that
+    /// this struct is not being misused.
+    pub fn unlock(self: *AccountLocks, accounts: []const struct { Pubkey, bool }) u64 {
+        var already_unlocked: u64 = 0;
+        for (accounts) |account| {
+            const address, const write = account;
+            if (write) {
+                // if (!self.write_locks.swapRemove(address)) already_unlocked += 1;
+                already_unlocked += unlockOneGeneric(&self.write_locks, address);
+            } else {
+                already_unlocked += unlockOneGeneric(&self.readonly_locks, address);
+            }
+        }
+        return already_unlocked;
+    }
+
+    /// returns 0 if it was still locked, 1 if it was already unlocked.
+    fn unlockOneGeneric(locks: *std.AutoArrayHashMapUnmanaged(Pubkey, u64), address: Pubkey) u64 {
+        const index = locks.getIndex(address) orelse {
+            return 1;
+        };
+
+        const value = &locks.entries.slice().items(.value)[index];
+        if (value.* == 0) {
+            // this means there is an internal bug within the unlock
+            // method, since the next block here should remove the item
+            // before this number would ever reach zero.
+            unreachable;
+        } else if (value.* == 1) {
+            locks.swapRemoveAt(index);
+        } else {
+            value.* -= 1;
+        }
+
+        return 0;
+    }
+};
