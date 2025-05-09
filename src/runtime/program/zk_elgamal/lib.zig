c6645f06b3f0a7d34d2d52004c83c9430bd6d275
David Rubin <daviru007@icloud.com>
2025-05-09 00:40:41 -0700

    intial zksdk

---
+++ src/runtime/program/zk_elgamal/lib.zig
@@ -0,0 +1,9 @@
+const sig = @import("../../../sig.zig");
+
+pub const ID =
+    sig.core.Pubkey.parseBase58String(
+        "ZkE1Gama1Proof11111111111111111111111111111",
+    ) catch unreachable;
+
+pub const ProofInstruction = @import("instruction.zig").ProofInstruction;
+pub const execute = @import("execute.zig").execute;
