d772d80370d5747cd5e48d72adf57e58ea1cf708
David Rubin <david@vortan.dev>
2025-05-15 01:40:00 -0700

    basic ciphertext-ciphertext equality proof

--- src/runtime/program/zk_elgamal/lib.zig
+++ src/runtime/program/zk_elgamal/lib.zig
@@ -2,8 +2,8 @@ const sig = @import("../../../sig.zig");
 
 pub const ID =
     sig.core.Pubkey.parseBase58String(
-        "ZkE1Gama1Proof11111111111111111111111111111",
-    ) catch unreachable;
+    "ZkE1Gama1Proof11111111111111111111111111111",
+) catch unreachable;
 
 pub const ProofInstruction = @import("instruction.zig").ProofInstruction;
 pub const execute = @import("execute.zig").execute;
