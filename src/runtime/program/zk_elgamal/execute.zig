2cf15a7fd74356178fdd3cbce1c910d8d4929a49
David Rubin <david@vortan.dev>
2025-05-14 21:23:13 -0700

    zero ciphertext proof

--- src/runtime/program/zk_elgamal/execute.zig
+++ src/runtime/program/zk_elgamal/execute.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const sig = @import("../../../sig.zig");
 
 const Pubkey = sig.core.Pubkey;
-const zk_elgamal_program = sig.runtime.program.zk_elgamal_program;
 const InstructionError = sig.core.instruction.InstructionError;
 const InstructionContext = sig.runtime.InstructionContext;
 
