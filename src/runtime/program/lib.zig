64d009a54deeab23384af43d7aaf5b8265f0e522
David Rubin <david@vortan.dev>
2025-05-15 20:12:07 -0700

    start on program intergration

--- src/runtime/program/lib.zig
+++ src/runtime/program/lib.zig
@@ -6,7 +6,7 @@ pub const bpf_loader_program = @import("bpf_loader/lib.zig");
 pub const precompile_programs = @import("precompiles/lib.zig");
 pub const system_program = @import("system/lib.zig");
 pub const testing = @import("testing.zig");
-pub const zk_elgamal_program = @import("zk_elgamal/lib.zig");
+pub const zk_elgamal = @import("zk_elgamal/lib.zig");
 pub const vote_program = @import("vote/lib.zig");
 
 pub const bpf = @import("bpf/lib.zig");
@@ -33,6 +33,7 @@ fn initProgramEntrypoints() std.StaticStringMap(EntrypointFn) {
         .{ system_program.ID.base58String().slice(), system_program.execute },
         .{ vote_program.ID.base58String().slice(), vote_program.execute },
         .{ address_lookup_table.ID.base58String().slice(), address_lookup_table.execute },
+        .{ zk_elgamal.ID.base58String().slice(), zk_elgamal.execute },
     });
 }
 
