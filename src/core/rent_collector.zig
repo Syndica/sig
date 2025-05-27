4d189a6b0ddd41ecd02de24c79e99b4c57b5b494
Sobeston <15335529+Sobeston@users.noreply.github.com>
2025-05-27 23:43:20 +0100

    get building again

--- src/core/rent_collector.zig
+++ src/core/rent_collector.zig
@@ -134,6 +134,50 @@ pub const RentCollector = struct {
 
         return .{ .Paying = due };
     }
+
+    pub const RentPaying = struct { lamports: u64, data_len: u64 };
+
+    pub const RentState = union(enum) {
+        /// account.lamports == 0
+        Uninitialized,
+        /// 0 < account.lamports < rent-exempt-minimum
+        RentPaying: RentPaying,
+        /// account.lamports >= rent-exempt-minimum
+        RentExempt,
+    };
+
+    pub fn getAccountRentState(self: RentCollector, lamports: u64, data_len: usize) RentState {
+        if (lamports == 0) return .Uninitialized;
+        if (self.rent.isExempt(lamports, data_len)) return .RentExempt;
+        return .{
+            .RentPaying = .{
+                .data_len = data_len,
+                .lamports = lamports,
+            },
+        };
+    }
+
+    pub fn transitionAllowed(pre: RentState, post: RentState) bool {
+        // can always transition away from RentPaying
+        if (post != .RentPaying) return true;
+        // can't transition an account into RentPaying
+        if (pre != .RentPaying) return false;
+
+        // can't remain RentPaying if resized or credited.
+        if (post.RentPaying.lamports > pre.RentPaying.lamports) return false;
+        if (post.RentPaying.data_len != pre.RentPaying.data_len) return false;
+
+        return true;
+    }
+
+    pub fn checkRentStateWithAccount(
+        pre: RentState,
+        post: RentState,
+        address: *const Pubkey,
+    ) error{InsufficientFundsForRent}!void {
+        if (sig.runtime.ids.Incinerator.equals(address)) return;
+        if (transitionAllowed(pre, post)) return error.InsufficientFundsForRent;
+    }
 };
 
 pub fn defaultCollector(epoch: Epoch) RentCollector {
