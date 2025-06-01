0cf859b9a0b159c778f82a94d7bf3a2e1cdc8e95
David Rubin <daviru007@icloud.com>
2025-05-31 17:51:26 -0700

    AVX512 MSM implementation

--- src/crypto/lib.zig
+++ src/crypto/lib.zig
@@ -1,1 +1,14 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
 pub const FnvHasher = @import("fnv.zig").FnvHasher;
+
+const vector = @import("vector.zig");
+const avx512 = @import("avx512.zig");
+const use_avx512 = builtin.cpu.arch == .x86_64 and
+    std.Target.x86.featureSetHas(builtin.cpu.features, .avx512ifma) and
+    std.Target.x86.featureSetHas(builtin.cpu.features, .avx512vl);
+
+const namespace = if (use_avx512) avx512 else vector;
+pub const ExtendedPoint = namespace.ExtendedPoint;
+pub const CachedPoint = namespace.CachedPoint;
