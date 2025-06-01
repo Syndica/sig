ea937df344bce8eedffe4a9061730defdeb89eeb
David Rubin <david@vortan.dev>
2025-06-01 09:08:59 -0700

    start cleanup

--- src/crypto/lib.zig
+++ src/crypto/lib.zig
@@ -5,11 +5,12 @@ pub const FnvHasher = @import("fnv.zig").FnvHasher;
 
 const vector = @import("vector.zig");
 const avx512 = @import("avx512.zig");
-const use_avx512 = builtin.cpu.arch == .x86_64 and
+const has_avx512 = builtin.cpu.arch == .x86_64 and
     std.Target.x86.featureSetHas(builtin.cpu.features, .avx512ifma) and
     std.Target.x86.featureSetHas(builtin.cpu.features, .avx512vl);
 
-const namespace = if (use_avx512) avx512 else vector;
+// avx512 implementation relies on llvm specific tricks
+const namespace = if (has_avx512 and builtin.zig_backend == .stage2_llvm) avx512 else vector;
 pub const ExtendedPoint = namespace.ExtendedPoint;
 pub const CachedPoint = namespace.CachedPoint;
 pub const pippenger = @import("pippenger.zig");
