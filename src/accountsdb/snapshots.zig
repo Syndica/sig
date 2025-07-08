4ddad8a85a6ec4aa82d58344a2556f6b6d9e4074
Harold Newman <hnewman2744@gmail.com>
2025-07-08 11:05:37 -0600

    replace versioned epoch stakes

--- src/accountsdb/snapshots.zig
+++ src/accountsdb/snapshots.zig
@@ -18,7 +18,7 @@ const Rent = sig.core.genesis_config.Rent;
 const Slot = sig.core.time.Slot;
 const SlotAndHash = sig.core.hash.SlotAndHash;
 const SlotHistory = sig.runtime.sysvar.SlotHistory;
-const VersionedEpochStakes = sig.core.NewVersionedEpochStakes;
+const VersionedEpochStakes = sig.core.VersionedEpochStakes;
 const UnixTimestamp = sig.core.UnixTimestamp;
 
 const FileId = sig.accounts_db.accounts_file.FileId;
