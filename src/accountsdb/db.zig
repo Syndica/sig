7a1b6ab7730f3311d444ed7608db966d6dd19876
Drew Nutter <dnut@users.noreply.github.com>
2025-07-15 21:08:34 -0400

    genericize account hashing

--- src/accountsdb/db.zig
+++ src/accountsdb/db.zig
@@ -1413,16 +1413,19 @@ pub const AccountsDB = struct {
                         account_hash = switch (account) {
                             .file => |in_file_account| blk: {
                                 var iter = in_file_account.data.iterator();
-                                break :blk sig.core.account.hashAccount(
+                                var hash = Hash.ZEROES;
+                                sig.core.account.hashAccount(
                                     in_file_account.lamports().*,
                                     &iter,
                                     &in_file_account.owner().data,
                                     in_file_account.executable().*,
                                     in_file_account.rent_epoch().*,
                                     &in_file_account.pubkey().data,
+                                    &hash.data,
                                 );
+                                break :blk hash;
                             },
-                            .unrooted_map => |unrooted_account| unrooted_account.hash(&key),
+                            .unrooted_map => |unrooted_account| unrooted_account.hash(Hash, &key),
                         };
                     }
                 }
