f8a4d4e82ee873ddb44e6ab8f7caf8c2efb04920
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-07-17 20:14:03 +0400

    Removed the conflict resolution todo check

--- src/runtime/program/vote/state.zig
+++ src/runtime/program/vote/state.zig
@@ -5050,7 +5050,7 @@ test "state.VoteState.checkAndFilterProposedVoteState slot hashes mismatch" {
     try std.testing.expectEqual(VoteError.slot_hash_mismatch, maybe_error);
 }
 
-fn processSlotVoteUnchecked(
+pub fn processSlotVoteUnchecked(
     allocator: std.mem.Allocator,
     vote_state: *VoteState,
     slot: Slot,
