cdbbfc02fa7984ad2c8f4bfbdd13d58594e84064
Trevor Berrange Sanchez <trevor.berrange@syndica.io>
2025-07-17 19:53:55 +0200

    Remove unused variable, run style script Unused var was accidentally left-over during rebase

--- src/consensus/replay_tower.zig
+++ src/consensus/replay_tower.zig
@@ -4,7 +4,6 @@ const sig = @import("../sig.zig");
 const AutoHashMapUnmanaged = std.AutoHashMapUnmanaged;
 const AutoArrayHashMapUnmanaged = std.AutoArrayHashMapUnmanaged;
 
-const AccountsDB = sig.accounts_db.AccountsDB;
 const BlockTimestamp = sig.runtime.program.vote.state.BlockTimestamp;
 const Hash = sig.core.Hash;
 const Lockout = sig.runtime.program.vote.state.Lockout;
