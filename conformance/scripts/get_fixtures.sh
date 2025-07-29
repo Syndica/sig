3d6f78b94780828f7bc0c38aee7adef8ea5eea27
David Rubin <david@vortan.dev>
2025-07-29 11:33:50 -0700

    enable more harnesses

--- conformance/scripts/get_fixtures.sh
+++ conformance/scripts/get_fixtures.sh
@@ -24,16 +24,41 @@ PASSING_DIRS=(
     "syscall/fixtures/try_find_program_address"
     "syscall/fixtures/vm"
 
-    "instr/fixtures/zk_sdk"
-
-    # "vm_interp/fixtures/latest"
-    "vm_interp/fixtures/v0"
-    "vm_interp/fixtures/v1"
-    # "vm_interp/fixtures/v2"
-    # "vm_interp/fixtures/v3"
-
     # Unimplemented
     # "syscall/fixtures/secp256k1"
+
+    "instr/fixtures/zk_sdk"
+    "instr/fixtures/unknown"
+    "instr/fixtures/compute-budget"
+
+    "instr/fixtures/bpf-address-lookup-table"
+    "instr/fixtures/bpf-config"
+    "instr/fixtures/bpf-loader"
+    "instr/fixtures/bpf-loader-v1-programs"
+    "instr/fixtures/bpf-loader-v2"
+    "instr/fixtures/bpf-loader-v2-programs"
+    "instr/fixtures/bpf-loader-v3"
+
+    # Passed: 0, Failed: 6934, Skipped: 0 (unimplemented)
+    # "instr/fixtures/stake"
+
+    # Passed: 7750, Failed: 5, Skipped: 0
+    # "instr/fixtures/vote"
+
+    # Passed: 5077, Failed: 248, Skipped: 0 (also takes a long time to run)
+    # "instr/fixtures/system"
+
+    # Passed: 461, Failed: 48, Skipped: 0
+    # "instr/fixtures/bpf-loader-v3-programs"
+
+    # Passed: 369, Failed: 2, Skipped: 0
+    # "instr/fixtures/bpf-loader-upgradeable-v1-programs"
+
+    "vm_interp/fixtures/latest"
+    "vm_interp/fixtures/v0"
+    "vm_interp/fixtures/v1"
+    "vm_interp/fixtures/v2"
+    "vm_interp/fixtures/v3"
 )
 
 for dir in "${PASSING_DIRS[@]}"; do
