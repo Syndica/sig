760e6fbd5b472321525f75967bd67812b0d2cca5
David Rubin <david@vortan.dev>
2025-07-29 17:29:09 -0700

    add transaction context definitions for the protosol

--- conformance/build.zig
+++ conformance/build.zig
@@ -20,10 +20,12 @@ pub fn build(b: *std.Build) void {
             "protosol/proto/elf.proto",
             "protosol/proto/vm.proto",
             "protosol/proto/shred.proto",
+            "protosol/proto/txn.proto",
         },
         .include_directories = &.{"protosol/proto"},
     });
 
+    // current commit: 0d695a1dac9c3e68b1ed8b3dc9cb30fe4d0a4b8f
     const proto_step = b.step("protobuf", "Generate the `protosol` directory");
     proto_step.dependOn(&protoc_step.step);
 
@@ -44,4 +46,21 @@ pub fn build(b: *std.Build) void {
 
     lib.root_module.fuzz = true;
     b.installArtifact(lib);
+
+    const run_tests = b.step("test", "Run unit tests");
+    const filters = b.option([]const []const u8, "filter", "") orelse &.{};
+    const unit_tests_exe = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/lib.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "sig", .module = sig.module("sig") },
+                .{ .name = "protobuf", .module = pb.module("protobuf") },
+            },
+        }),
+        .filters = filters,
+    });
+    const run = b.addRunArtifact(unit_tests_exe);
+    run_tests.dependOn(&run.step);
 }
