cd006966abd74d33074060cfe4adee3ff74534d9
David Rubin <david@vortan.dev>
2025-09-12 18:36:00 -0700

    add naive setup to eddsa benchmark

--- src/crypto/benchmark.zig
+++ src/crypto/benchmark.zig
@@ -7,6 +7,21 @@ pub const Benchmark = struct {
     pub const min_iterations = 100;
     pub const max_iterations = 1_000;
 
+    pub fn naiveBatchVerify() !sig.time.Duration {
+        const message = "test!";
+
+        const keypair = Ed25519.KeyPair.generate();
+        const signature = try keypair.sign(message, null);
+
+        const inputs: [100]Ed25519.Signature = @splat(signature);
+
+        var start = try sig.time.Timer.start();
+        for (inputs) |s| {
+            std.mem.doNotOptimizeAway(try s.verify(message, keypair.public_key));
+        }
+        return start.read();
+    }
+
     pub fn stdBatchVerify() !sig.time.Duration {
         const message = "test!";
 
