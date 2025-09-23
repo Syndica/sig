34fd91ef0185f31e5de9a223d54878196b4b653f
kprotty <kbutcher6200@gmail.com>
2025-09-23 06:25:24 -0500

    loader v4

--- src/runtime/program/bpf_loader/v4_state.zig
+++ src/runtime/program/bpf_loader/v4_state.zig
@@ -9,6 +9,8 @@ pub const Status = enum(u64) {
     deployed,
     /// Same as `Deployed`, but can not be retracted anymore
     finalized,
+
+    pub const BincodeSize = u64;
 };
 
 /// [agave] https://github.com/anza-xyz/solana-sdk/blob/c7c8c604991bf5d1e4441f32659546c84a17d92c/loader-v4-interface/src/state.rs#L19
