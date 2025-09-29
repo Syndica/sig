be5589e753f2862c3bfe17fec96c1bade71c676b
David Rubin <david@vortan.dev>
2025-09-29 02:57:10 -0700

    add missing hash

--- conformance/src/txn_execute.zig
+++ conformance/src/txn_execute.zig
@@ -1403,6 +1403,7 @@ pub fn hashSlot(
         const delta_hash = try freeze.deltaMerkleHash(account_reader, allocator, 0);
         hash.update(&delta_hash.data);
     }
+    hash.update(&signature_count_bytes);
     hash.update(&blockhash.data);
     const initial_hash = hash.finalResult();
 
