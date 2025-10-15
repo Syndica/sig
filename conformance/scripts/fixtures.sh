6c6f895813fd35178e46da2af80324c75fc45a14
Drew Nutter <dnut@users.noreply.github.com>
2025-10-15 08:31:05 -0400

    feat(conformance): add scripts to simplify locally running conformance

--- conformance/scripts/fixtures.sh
+++ conformance/scripts/fixtures.sh
@@ -1,73 +1,25 @@
 #!/bin/bash
 
-PASSING_DIRS=(
-    "elf_loader/fixtures"
+PASSING_DIRS=()
 
-    "vm_interp/fixtures/v0"
-    "vm_interp/fixtures/v1"
-    "vm_interp/fixtures/v2"
-    "vm_interp/fixtures/latest"
-
-    "syscall/fixtures/abort"
-    "syscall/fixtures/alt_bn128"
-    "syscall/fixtures/blake3"
-    "syscall/fixtures/cpi"
-    "syscall/fixtures/create_program_address"
-    "syscall/fixtures/curve25519"
-    "syscall/fixtures/get_epoch_schedule"
-    "syscall/fixtures/get_return_data"
-    "syscall/fixtures/keccak256"
-    "syscall/fixtures/log"
-    "syscall/fixtures/log_data"
-    "syscall/fixtures/memcmp"
-    "syscall/fixtures/memcpy"
-    "syscall/fixtures/memmove"
-    "syscall/fixtures/memset"
-    "syscall/fixtures/panic"
-    "syscall/fixtures/poseidon"
-    "syscall/fixtures/sha256"
-    "syscall/fixtures/sol_get_sysvar"
-    "syscall/fixtures/stack_height"
-    "syscall/fixtures/try_find_program_address"
-    "syscall/fixtures/vm"
-    "syscall/fixtures/secp256k1"
-
-    "instr/fixtures/zk_sdk"
-    "instr/fixtures/unknown"    
-    "instr/fixtures/compute-budget"
-    "instr/fixtures/stake"
-    "instr/fixtures/vote"
-    "instr/fixtures/system"
-    
-    "instr/fixtures/bpf-address-lookup-table"
-    "instr/fixtures/bpf-config"
-    "instr/fixtures/bpf-loader"
-    "instr/fixtures/bpf-loader-v1-programs"
-    "instr/fixtures/bpf-loader-v2"
-    "instr/fixtures/bpf-loader-v2-programs"
-    "instr/fixtures/bpf-loader-v3"
-    "instr/fixtures/bpf-loader-v3-programs"
-    "instr/fixtures/bpf-loader-upgradeable-v1-programs"
-
-    "txn/fixtures/programs"
-
-    "txn/fixtures/precompile/ed25519"
-    "txn/fixtures/precompile/secp256k1"
-    "txn/fixtures/precompile/secp256r1"
-)
+while IFS= read -r line; do
+    if [[ -n "$line" ]]; then
+        PASSING_DIRS+=("$line")
+    fi
+done < "$(dirname "${BASH_SOURCE[0]}")/fixtures.txt"
 
 FIXTURES=()
 
 for dir in "${PASSING_DIRS[@]}"; do
-  while IFS= read -r -d '' file; do
-    FIXTURES+=("$file")
-  done < <(find "./test-vectors/$dir" -type f -name '*.fix' -print0)
+    while IFS= read -r -d '' file; do
+        FIXTURES+=("$file")
+    done < <(find "./test-vectors/$dir" -type f -name '*.fix' -print0)
 done
 
 mkdir -p test-inputs/
 printf "%s\n" "${FIXTURES[@]}" \
-  | circleci tests split \
-  | xargs -d '\n' -I{} cp "{}" test-inputs/
+    | circleci tests split \
+    | xargs -d '\n' -I{} cp "{}" test-inputs/
 
 cd solana-conformance
 source test_suite_env/bin/activate
@@ -77,12 +29,12 @@ export ASAN_OPTIONS=detect_leaks=0
 
 echo "Generating fixtures"
 solana-test-suite create-fixtures \
--s ../libsolfuzz_agave.so \
--i "../test-inputs" \
--o "test_fixtures/"
+    -s ../libsolfuzz_agave.so \
+    -i "../test-inputs" \
+    -o "test_fixtures/"
 
 echo "Running fixtures"
 solana-test-suite exec-fixtures \
--t ../conformance/zig-out/lib/libsolfuzz_sig.so \
--o "test_results/" \
--i test_fixtures/ | tee /dev/tty | grep -q "Failed: 0,"
+    -t ../conformance/zig-out/lib/libsolfuzz_sig.so \
+    -o "test_results/" \
+    -i test_fixtures/ | tee /dev/tty | grep -q "Failed: 0,"
