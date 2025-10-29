3a5d092290328acfa3f351fd7c9e59bf92125b1d
Drew Nutter <dnut@users.noreply.github.com>
2025-10-29 13:40:08 -0400

    ci: fix log, should append

--- ci/run-and-update-service/run.sh
+++ ci/run-and-update-service/run.sh
@@ -3,4 +3,4 @@
 /home/sig/sig/zig-out/bin/sig $@ 2>>/home/sig/sig/logs/sig.log >>/home/sig/sig/logs/sig.log
 
 timestamp="$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")"
-echo "time=$timestamp level=error message=\"exited unexpectedly\"" >/home/sig/sig/logs/sig.log
+echo "time=$timestamp level=error message=\"exited unexpectedly\"" >>/home/sig/sig/logs/sig.log
