8c70a38e623a70b9f5e624e05f915fc25e06d4d4
David Rubin <david@vortan.dev>
2025-10-30 18:50:44 -0700

    version: add `Clients` and set our version to `Sig`

--- src/version/version.zig
+++ src/version/version.zig
@@ -10,7 +10,7 @@ pub const ClientVersion = struct {
     patch: u16,
     commit: u32, // first 4 bytes of the sha1 commit hash
     feature_set: u32, // first 4 bytes of the FeatureSet identifier
-    client: u16,
+    client: Clients,
 
     pub const CURRENT: ClientVersion = .{
         .major = version.major,
@@ -19,11 +19,38 @@ pub const ClientVersion = struct {
         .commit = std.fmt.parseInt(u32, version.build orelse "0", 16) catch
             @compileError("failed to parse build"),
         .feature_set = 0,
-        .client = 1,
+        .client = .sig,
+    };
+
+    const Clients = enum(u16) {
+        solana_labs = 0,
+        jito_labs = 1,
+        frankendancer = 2,
+        agave = 3,
+        agave_paladin = 4,
+        agave_bam = 5,
+        sig = 6,
+        firedancer = 7,
+        _,
+
+        pub fn serialize(writer: anytype, data: anytype, _: sig.bincode.Params) !void {
+            try std.leb.writeUleb128(writer, @intFromEnum(data));
+        }
+
+        pub fn deserialize(
+            _: *sig.bincode.LimitAllocator,
+            reader: anytype,
+            _: sig.bincode.Params,
+        ) !Clients {
+            return @enumFromInt(try std.leb.readUleb128(u16, reader));
+        }
     };
 
     pub const @"!bincode-config:major" = sig.bincode.VarIntConfig(u16);
     pub const @"!bincode-config:minor" = sig.bincode.VarIntConfig(u16);
     pub const @"!bincode-config:patch" = sig.bincode.VarIntConfig(u16);
-    pub const @"!bincode-config:client" = sig.bincode.VarIntConfig(u16);
+    pub const @"!bincode-config:client" = sig.bincode.FieldConfig(Clients){
+        .serializer = Clients.serialize,
+        .deserializer = Clients.deserialize,
+    };
 };
