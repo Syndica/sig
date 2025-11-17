1e39ff0a40f51a84b8e65cd0cacf44464994d2eb
Drew Nutter <dnut@users.noreply.github.com>
2025-11-17 15:09:24 -0500

    ci: clear validator state on *any* restart, not just update restarts

--- ci/run-and-update-service/run.sh
+++ ci/run-and-update-service/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf /home/sig/sig/validator  # needed until we have a resumable accountsdb
+
 /home/sig/sig/zig-out/bin/sig $@ 2>>/home/sig/sig/logs/sig.log >>/home/sig/sig/logs/sig.log
 
 timestamp="$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")"
