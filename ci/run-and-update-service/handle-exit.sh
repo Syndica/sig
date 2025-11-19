92d29c3c3d9b8ff9b8e9a54b130debe3e958127d
Drew Nutter <dnut@users.noreply.github.com>
2025-11-19 17:56:19 -0500

    ci: move validator archive folder outside repo

--- ci/run-and-update-service/handle-exit.sh
+++ ci/run-and-update-service/handle-exit.sh
@@ -13,8 +13,8 @@ echo "time=$timestamp level=error scope=handle-exit message=\"$message\"" >>/hom
 # Sig crashed, so we want to preserve the state for debugging, but we also want
 # to clear it out from "validator" so the next run can start fresh.
 if [ -d /home/sig/sig/validator ]; then
-    mkdir -p /home/sig/sig/validator-archive
+    mkdir -p /home/sig/validator-archive
     # Delete folders older than 4 days to avoid filling up the disk.
-    find /home/sig/sig/validator-archive -mindepth 1 -maxdepth 1 -type d -mtime +4 -exec rm -rf {} \;
-    mv /home/sig/sig/validator "/home/sig/sig/validator-archive/$(date -u +"%Y-%m-%dT%H:%M:%S")"
+    find /home/sig/validator-archive -mindepth 1 -maxdepth 1 -type d -mtime +4 -exec rm -rf {} \;
+    mv /home/sig/sig/validator "/home/sig/validator-archive/$(date -u +"%Y-%m-%dT%H:%M:%S")"
 fi
