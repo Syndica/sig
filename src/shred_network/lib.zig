991a0c69c69cf029251cfd2bb61a658825f6db5b
oluwadadepo aderemi <272535+dadepo@users.noreply.github.com>
2025-11-24 18:32:53 +0400

    Move duplicate handling to its own file

--- src/shred_network/lib.zig
+++ src/shred_network/lib.zig
@@ -4,6 +4,7 @@ pub const shred_receiver = @import("collector/shred_receiver.zig");
 pub const shred_tracker = @import("collector/shred_tracker.zig");
 pub const shred_verifier = @import("collector/shred_verifier.zig");
 
+pub const duplicate_shred_handler = @import("collector/duplicate_shred_handler.zig");
 pub const duplicate_shred_listener = @import("duplicate_shred_listener.zig");
 
 pub const shred_deduper = @import("transmitter/shred_deduper.zig");
