e3962a7a2049c075b0e1cfb6004d952827ac8483
David Rubin <david@vortan.dev>
2025-11-26 07:25:45 -0800

    cleanup identity kp code

--- src/identity.zig
+++ src/identity.zig
@@ -3,11 +3,9 @@ const sig = @import("sig.zig");
 
 const Logger = sig.trace.Logger("identity");
 const SecretKey = std.crypto.sign.Ed25519.SecretKey;
+pub const KeyPair = std.crypto.sign.Ed25519.KeyPair;
 
 pub const IDENTITY_KEYPAIR_PATH = "identity.key";
-
-/// Re-export of stdlib's Ed25519 KeyPair.
-pub const KeyPair = std.crypto.sign.Ed25519.KeyPair;
 
 pub const ValidatorIdentity = struct {
     /// Public key identifying this validator
@@ -23,81 +21,67 @@ pub const SigningKeys = struct {
     authorized_voters: []const KeyPair,
 };
 
-/// Returns the keypair from {app data directory}/{IDENTITY_KEYPAIR_PATH} or creates a new one
+/// Returns the keypair from $APP_DATA/IDENTITY_KEYPAIR_PATH or creates a new one
 /// if the file does not exist. If the file is invalid, an error is returned.
 pub fn getOrInit(allocator: std.mem.Allocator, logger: Logger) !KeyPair {
-    const app_data_dir_path = try std.fs.getAppDataDir(allocator, "sig");
-    defer allocator.free(app_data_dir_path);
+    const data_directory_path = try std.fs.getAppDataDir(allocator, "sig");
+    defer allocator.free(data_directory_path);
 
-    if (!std.fs.path.isAbsolute(app_data_dir_path)) {
-        return error.DataDirPathIsNotAbsolute;
-    }
+    logger.info().logf(
+        "searching for identity key at {s}/{s}",
+        .{ data_directory_path, IDENTITY_KEYPAIR_PATH },
+    );
 
-    var app_data_dir = try std.fs.cwd().makeOpenPath(app_data_dir_path, .{});
-    defer app_data_dir.close();
+    var data_directory = try std.fs.cwd().makeOpenPath(data_directory_path, .{});
+    defer data_directory.close();
 
-    if (app_data_dir.openFile(IDENTITY_KEYPAIR_PATH, .{
-        // NOTE: the file will never be modified
-        .mode = .read_only,
-    })) |file| {
-        defer file.close();
-
-        var buf: [SecretKey.encoded_length]u8 = undefined;
-
-        const end_pos = try file.getEndPos();
-        if (end_pos != buf.len) {
-            logger.err().logf(
-                "Overlong identity file, expected {} bytes, found {}",
-                .{ buf.len, end_pos },
-            );
-            return error.InvalidIdentityFile;
-        }
-
-        const file_len = try file.readAll(&buf);
-        if (file_len != buf.len) {
-            logger.err().logf(
-                "Truncated identity file, expected {} bytes, found {}",
-                .{ buf.len, file_len },
-            );
-            return error.InvalidIdentityFile;
-        }
-
-        // NOTE: this should never fail so we can ignore the error
-        const secret_key = SecretKey.fromBytes(buf) catch |err| switch (err) {};
-        const keypair = try KeyPair.fromSecretKey(secret_key);
-
-        return keypair;
-    } else |err| switch (err) {
-        else => |e| return e,
-        error.FileNotFound => {
-            // create the file with a new keypair
-            const file = try app_data_dir.createFile(IDENTITY_KEYPAIR_PATH, .{
-                .truncate = true,
-            });
-            defer file.close();
-
-            const keypair = KeyPair.generate();
-            try file.writeAll(&keypair.secret_key.toBytes());
-
-            return keypair;
+    const file = try data_directory.createFile(
+        IDENTITY_KEYPAIR_PATH,
+        .{
+            .read = true,
+            .truncate = false,
+            .lock = .exclusive, // no way to check size twice atomically without a lock
         },
+    );
+    defer file.close();
+
+    const size = (try file.stat()).size;
+    if (size == 0) {
+        const new_kp: KeyPair = .generate();
+        try file.writeAll(&new_kp.secret_key.toBytes());
+        return new_kp;
+    } else if (size == SecretKey.encoded_length) {
+        var buffer: [SecretKey.encoded_length]u8 = undefined;
+        const length = try file.readAll(&buffer);
+        std.debug.assert(buffer.len == length);
+
+        const secret_key: SecretKey = .{ .bytes = buffer };
+        return try KeyPair.fromSecretKey(secret_key);
+    } else {
+        logger.err().logf(
+            "identity file found was the wrong size, expected 64 byte secret key, found {d}",
+            .{size},
+        );
+        return error.InvalidIdentityFile;
     }
 }
 
 /// Reads a binary keypair file and returns its public key as `Pubkey`.
 pub fn readBinaryKeypairPubkey(path: []const u8) !sig.core.Pubkey {
-    var file = try std.fs.cwd().openFile(path, .{});
+    var file = try std.fs.cwd().openFile(
+        path,
+        .{ .lock = .exclusive }, // need exclusive lock in order to check size
+    );
     defer file.close();
 
-    var buf: [std.crypto.sign.Ed25519.SecretKey.encoded_length]u8 = undefined;
+    const size = (try file.stat()).size;
+    if (size != SecretKey.encoded_length) return error.InvalidKeypairFile;
 
-    const end_pos = try file.getEndPos();
-    if (end_pos != buf.len) return error.InvalidKeypairFile;
+    var buffer: [SecretKey.encoded_length]u8 = undefined;
+    const length = try file.readAll(&buffer);
+    std.debug.assert(length == buffer.len);
 
-    const file_len = try file.readAll(&buf);
-    if (file_len != buf.len) return error.InvalidKeypairFile;
-
-    const secret_key = try std.crypto.sign.Ed25519.SecretKey.fromBytes(buf);
-    const keypair = try std.crypto.sign.Ed25519.KeyPair.fromSecretKey(secret_key);
+    const secret_key: SecretKey = .{ .bytes = buffer };
+    const keypair = try KeyPair.fromSecretKey(secret_key);
     return sig.core.Pubkey.fromPublicKey(&keypair.public_key);
 }
