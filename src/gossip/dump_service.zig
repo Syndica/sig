c4ca78752cc93f95cf513b0394738cc35a6d2a8b
InKryption <inkryption07@gmail.com>
2025-12-03 20:40:42 +0100

    Improve/simplify gossip dump logic * Use buffered file writer instead of allocating the whole thing. * Dump TPU information in "extra" column for `[Legacy]ContactInfo`s. * Use some comptime logic to write `Row`s in a structured way.

--- src/gossip/dump_service.zig
+++ src/gossip/dump_service.zig
@@ -44,62 +44,141 @@ pub const GossipDumpService = struct {
     }
 
     fn dumpGossip(self: *const Self, dir: std.fs.Dir, start_time: i64) !void {
-        const data = blk: {
-            var gossip_table_lock = self.gossip_table_rw.read();
-            defer gossip_table_lock.unlock();
-            const gossip_table: *const GossipTable = gossip_table_lock.get();
+        const now = std.time.timestamp();
+        const filename_bounded = sig.utils.fmt.boundedFmt(
+            "gossip-dump-{d:0>2}:{d:0>2}:{d:0>2}.csv",
+            secondsToHourMinuteSeconds(@intCast(now)),
+        );
 
-            // allocate buffer to write records
+        const file = try dir.createFile(filename_bounded.constSlice(), .{});
+        defer file.close();
+
+        var bws = std.io.bufferedWriter(file.writer());
+        defer bws.flush() catch |err| self.logger.warn().logf(
+            "{s}: Failed to flush gossip dump in defer.",
+            .{@errorName(err)},
+        );
+        const writer = bws.writer();
+
+        const Row = struct {
+            message_type: sig.gossip.data.GossipDataTag,
+            pubkey: sig.core.Pubkey,
+            hash: sig.core.Hash,
+            wallclock: u64,
+            shred_version: ?u16,
+            gossip_addr: ?sig.net.SocketAddr,
+            extra: Extra,
+
+            const Extra = union(enum) {
+                null,
+                legacy_contact_info: struct {
+                    tpu: ?sig.net.SocketAddr,
+                    tpu_vote: ?sig.net.SocketAddr,
+                },
+                contact_info: struct {
+                    tpu: ?sig.net.SocketAddr,
+                    tpu_vote: ?sig.net.SocketAddr,
+                    tpu_quic: ?sig.net.SocketAddr,
+                    tpu_vote_quic: ?sig.net.SocketAddr,
+                },
+            };
+        };
+
+        // output csv header
+        inline for (@typeInfo(Row).@"struct".fields, 0..) |s_field, i| {
+            if (i != 0) try writer.writeByte(',');
+            try writer.writeAll(s_field.name);
+        }
+        try writer.writeByte('\n');
+
+        {
+            const gossip_table, var gossip_table_lg = self.gossip_table_rw.readWithLock();
+            defer gossip_table_lg.unlock();
+
             const table_len = gossip_table.store.count();
-            const buf = try self.allocator.alloc(u8, (1 + table_len) * 200);
-            errdefer self.allocator.free(buf);
-            var stream = std.io.fixedBufferStream(buf);
-            const writer = stream.writer();
+
+            self.logger.info().logf("gossip table size at {}s: {}", .{
+                now -| start_time,
+                table_len,
+            });
 
             // write records to string
-            const endec = base58.Table.BITCOIN;
-            var iterator = gossip_table.store.iterator();
-            while (iterator.next()) |entry| {
+            var gossip_iter = gossip_table.store.iterator();
+            while (gossip_iter.next()) |entry| {
                 const gossip_versioned_data = entry.getVersionedData();
                 const val: SignedGossipData = gossip_versioned_data.signedData();
 
-                var encoded_buf: [52]u8 = undefined;
-                const encoded_len = endec.encode(
-                    &encoded_buf,
-                    &gossip_versioned_data.metadata.value_hash.data,
-                );
-                const encoded = encoded_buf[0..encoded_len];
+                const row: Row = .{
+                    .message_type = val.data,
+                    .pubkey = val.id(),
+                    .hash = gossip_versioned_data.metadata.value_hash,
+                    .wallclock = val.wallclock(),
+                    .shred_version = val.data.shredVersion(),
+                    .gossip_addr = val.data.gossipAddr(),
+                    .extra = switch (val.data) {
+                        .LegacyContactInfo => |lci| .{
+                            .legacy_contact_info = .{
+                                .tpu = lci.tpu,
+                                .tpu_vote = lci.tpu_vote,
+                            },
+                        },
+                        .ContactInfo => |ci| .{
+                            .contact_info = .{
+                                .tpu = ci.getSocket(.tpu),
+                                .tpu_vote = ci.getSocket(.tpu_vote),
+                                .tpu_quic = ci.getSocket(.tpu_quic),
+                                .tpu_vote_quic = ci.getSocket(.tpu_vote_quic),
+                            },
+                        },
+                        else => .null,
+                    },
+                };
+                inline for (@typeInfo(Row).@"struct".fields, 0..) |s_field, i| {
+                    const field_value = @field(row, s_field.name);
 
-                try writer.print(
-                    "{s},{s},{s},{},",
-                    .{ @tagName(val.data), val.id(), encoded, val.wallclock() },
-                );
-                if (val.data.gossipAddr()) |addr| {
-                    try addr.toAddress().format("", .{}, writer);
+                    if (i != 0) try writer.writeByte(',');
+                    switch (s_field.type) {
+                        sig.gossip.data.GossipDataTag,
+                        => try writer.print("{s}", .{@tagName(field_value)}),
+
+                        sig.core.Pubkey,
+                        sig.core.Hash,
+                        => try writer.print("{f}", .{field_value}),
+
+                        ?sig.net.SocketAddr,
+                        => try writer.print("{?f}", .{field_value}),
+
+                        u64 => try writer.print("{d}", .{field_value}),
+                        ?u16 => try writer.print("{?d}", .{field_value}),
+
+                        Row.Extra => switch (field_value) {
+                            .null => try writer.writeAll("null&null&null&null"),
+                            .legacy_contact_info => |lci| try writer.print(
+                                "{?f}&{?f}&null&null",
+                                .{ lci.tpu, lci.tpu_vote },
+                            ),
+                            .contact_info => |ci| try writer.print(
+                                "{?f}&{?f}&{?f}&{?f}",
+                                .{ ci.tpu, ci.tpu_vote, ci.tpu_quic, ci.tpu_vote_quic },
+                            ),
+                        },
+                        else => @compileError("Unhandled " ++ @typeName(s_field.type)),
+                    }
                 }
-                try writer.writeAll(",");
-                if (val.data.shredVersion()) |shred| {
-                    try writer.print("{}", .{shred});
-                }
-                try writer.writeAll("\n");
+                try writer.writeByte('\n');
             }
-            break :blk .{ .buf = buf, .buf_len = stream.pos, .table_len = table_len };
-        };
-        defer self.allocator.free(data.buf);
+        }
 
-        // create file
-        const now = std.time.timestamp();
-        const filename_bounded = sig.utils.fmt.boundedFmt("gossip-dump-{}.csv", .{now});
-
-        var file = try dir.createFile(filename_bounded.constSlice(), .{});
-        defer file.close();
-
-        // output results
-        try file.writeAll("message_type,pubkey,hash,wallclock,gossip_addr,shred_version\n");
-        try file.writeAll(data.buf[0..data.buf_len]);
-        self.logger.info().logf("gossip table size at {}s: {}", .{
-            now -| start_time,
-            data.table_len,
-        });
+        try bws.flush();
     }
 };
+
+fn secondsToHourMinuteSeconds(secs: u64) struct { u5, u6, u6 } {
+    const epoch_secs: std.time.epoch.EpochSeconds = .{ .secs = secs };
+    const day_secs = epoch_secs.getDaySeconds();
+    return .{
+        day_secs.getHoursIntoDay(),
+        day_secs.getMinutesIntoHour(),
+        day_secs.getSecondsIntoMinute(),
+    };
+}
