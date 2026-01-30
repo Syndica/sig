10d06c0b8acb0dcf9679ad92e8697e164d2e5285
InKryption <trevor.berrange@syndica.io>
2026-01-30 15:17:03 +0100

    shred_network cli: take genesis file path

--- src/cmd.zig
+++ src/cmd.zig
@@ -159,6 +159,7 @@ pub fn main() !void {
             params.gossip_base.apply(&current_config);
             params.gossip_node.apply(&current_config);
             params.repair.apply(&current_config);
+            current_config.genesis_file_path = params.genesis_file_path;
             current_config.shred_network.dump_shred_tracker = params.repair.dump_shred_tracker;
             current_config.turbine.overwrite_stake_for_testing =
                 params.overwrite_stake_for_testing;
@@ -796,6 +797,7 @@ const Cmd = struct {
         gossip_base: GossipArgumentsCommon,
         gossip_node: GossipArgumentsNode,
         repair: RepairArgumentsBase,
+        genesis_file_path: ?[]const u8,
         /// TODO: Remove when no longer needed
         overwrite_stake_for_testing: bool,
         no_retransmit: bool,
@@ -823,6 +825,7 @@ const Cmd = struct {
                 .gossip_base = GossipArgumentsCommon.cmd_info,
                 .gossip_node = GossipArgumentsNode.cmd_info,
                 .repair = RepairArgumentsBase.cmd_info,
+                .genesis_file_path = genesis_file_path_arg,
                 .overwrite_stake_for_testing = .{
                     .kind = .named,
                     .name_override = null,
@@ -1565,6 +1568,7 @@ fn shredNetwork(
     }
 
     const genesis_path = try cfg.genesisFilePath() orelse
+        cfg.genesis_file_path orelse
         return error.GenesisPathNotProvided;
     const genesis_config = try GenesisConfig.init(allocator, genesis_path);
 
