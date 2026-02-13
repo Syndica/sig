7d843701a40202a801fab5e619ae2a87ba6bbca4
prestonsn <sundar.preston.789@gmail.com>
2026-02-13 17:52:13 +0000

    fix(consensus): TowerConsensus does not need mutable access to SlotTracker

--- src/replay/consensus/core.zig
+++ src/replay/consensus/core.zig
@@ -136,7 +136,7 @@ pub const TowerConsensus = struct {
             signing: sig.identity.SigningKeys,
             account_reader: AccountReader,
             ledger: *sig.ledger.Ledger,
-            slot_tracker: *SlotTracker,
+            slot_tracker: *const SlotTracker,
             /// Usually `.now()`.
             now: sig.time.Instant,
             registry: *sig.prometheus.Registry(.{}),
