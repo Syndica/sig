f1062f6a98d8e75eab3dcb7a690e5308d54f01dc
Drew Nutter <dnut@users.noreply.github.com>
2026-03-13 17:20:06 -0600

    fix(conformance): ci-run.sh

--- conformance/scripts/ci-run.sh
+++ conformance/scripts/ci-run.sh
@@ -1,50 +1,44 @@
 #!/usr/bin/env bash
+#
 # This script runs the `solana_conformance` step in CI.
+#
+# To run locally, set these variables:
+#   export SPLIT_TESTS=false
+#   export PREBUILT_LIB_DIR=zig-out/lib
+#
 set -euxo pipefail
 
+SPLIT_TESTS=${SPLIT_TESTS:-true}
 PREBUILT_LIB_DIR=${PREBUILT_LIB_DIR:-../workspace/conformance-release/lib}
 NUM_THREADS=${NUM_THREADS:-$(nproc || sysctl -n hw.ncpu || echo 1)}
 
 conformance_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd $conformance_dir
-. commits.env
 
-echo Setup solana-conformance
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install -y python3.11 python3.11-dev python3.11-venv rename gcc
-scripts/setup-env.sh get-solana-conformance
-scripts/setup-env.sh get-test-vectors
+echo Getting dependencies
+if command -v apt &> /dev/null; then
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt install -y python3.11 python3.11-dev python3.11-venv rename gcc
+fi
+scripts/setup-env.sh
 
-echo Parse fixture lists
-PASSING_DIRS=()
+echo Selecting a subset of fixtures to run
+rm -rf env/split-fixtures/
+mkdir -p env/split-fixtures/
+# Finds all .fix files in test-vectors, excludes known failures, splits across
+# CI nodes, and links the selected subset into split-fixtures
+comm -23 \
+    <(find "env/test-vectors/" \
+        -path 'env/test-vectors/block/*' -prune \
+        -o -type f -name '*.fix' -printf '%P\n' | sort) \
+    <(sort scripts/failing.txt) \
+    | if [ "$SPLIT_TESTS" == "true" ]; then circleci tests split; else cat; fi \
+    | sed 's_^_env/test-vectors/_' \
+    | xargs -d '\n' ln -t env/split-fixtures/
 
-set +x
-while IFS= read -r line; do
-    if [[ -n "$line" ]]; then
-        PASSING_DIRS+=("$line")
-    fi
-done < "scripts/fixtures.txt"
-
-FIXTURES=()
-
-for dir in "${PASSING_DIRS[@]}"; do
-    while IFS= read -r -d '' file; do
-        FIXTURES+=("$file")
-    done < <(find "test-vectors/$dir" -type f -name '*.fix' -print0)
-done
-
-mkdir -p split-fixtures/
-printf "%s\n" "${FIXTURES[@]}" \
-    | circleci tests split \
-    | xargs -d '\n' -I{} cp "{}" split-fixtures/
-set -x
-
-export LD_PRELOAD=/lib/x86_64-linux-gnu/libasan.so.8
-export ASAN_OPTIONS=detect_leaks=0
-
-echo "Running fixtures"
+echo Running fixtures
 env/pyvenv/bin/solana-conformance exec-fixtures \
     --num-processes $NUM_THREADS \
     -t ${PREBUILT_LIB_DIR}/libsolfuzz_sig.so \
     -o test_results/ \
-    -i split-fixtures/ | tee /dev/tty | grep -q "Failed: 0,"
+    -i env/split-fixtures/ | tee /dev/tty | grep -q "Failed: 0,"
