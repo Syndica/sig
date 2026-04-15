a1e5fc229c856c206f3021e6bbdb63b237d818fb
InKryption <trevor.berrange@syndica.io>
2026-04-15 14:34:10 +0200

    Make handle-exit.sh archive removal more robust

--- ci/run-and-update-service/handle-exit.sh
+++ ci/run-and-update-service/handle-exit.sh
@@ -18,22 +18,27 @@ fi
 
 log error "exited unexpectedly with SERVICE_RESULT=$SERVICE_RESULT EXIT_CODE=$EXIT_CODE EXIT_STATUS=$EXIT_STATUS"
 
-# Sig crashed, so we want to preserve the state for debugging, but we also want
-# to clear it out from "validator" so the next run can start fresh.
-if [ -d "$BASE_DIR/validator" ]; then
-
-    # compress validator state to a tarball
-    cp "$BASE_DIR/logs/sig.log" "$BASE_DIR/validator/"
-    timestamp="$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")"
-    tar --zstd -cf "$BASE_DIR/validator-${timestamp}.tar.zst" -C "$BASE_DIR" validator
-    rm -rf "$BASE_DIR/validator"
-
+upload_file() { local file_path=$1;
     # upload artifact to S3 and clean up local state
     . /etc/sig.conf  # get S3_BUCKET
     if [ -z "${S3_BUCKET:-}" ]; then
         log error "S3_BUCKET is not set, cannot upload validator archive"
         exit 1
     fi
-    aws s3 cp "$BASE_DIR/validator-${timestamp}.tar.zst" "s3://$S3_BUCKET/ci-crashes/validator-${timestamp}.tar.zst"
-    rm "$BASE_DIR/validator-${timestamp}.tar.zst"
+    aws s3 cp $file_path "s3://$S3_BUCKET/ci-crashes/$(basename $file_path)" || log error "failed to upload validator archive to s3 bucket"
+}
+
+# Sig crashed, so we want to preserve the state for debugging, but we also want
+# to clear it out from "validator" so the next run can start fresh.
+if [ -d "$BASE_DIR/validator" ]; then
+
+    # compress validator state to a tarball
+    cp "$BASE_DIR/logs/sig.log" "$BASE_DIR/validator/"
+    local timestamp="$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")"
+    local archive_name="validator-${timestamp}.tar.zst";
+    local archive_path="$BASE_DIR/$archive_name"
+    tar --zstd -cf $archive_path -C "$BASE_DIR" validator
+    rm -rf "$BASE_DIR/validator"
+    upload_file $archive_path || true
+    rm $archive_path
 fi
