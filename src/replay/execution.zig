9e67cd358c78747bcfa570599db1bdfd45b508c2
ajw221 <ajw221@gmail.com>
2026-05-14 12:47:34 -0400

    feat(simd-0242): require static nonce account (#1424)
    https://github.com/solana-foundation/solana-improvement-documents/blob/main/proposals/0242-static-nonce-account.md
    
    feature key: `7VVhpg5oAjAmnmz1zCcSHb2Z9ecZB2FQqpnEwReka9Zm`

--- src/replay/execution.zig
+++ src/replay/execution.zig
@@ -371,19 +371,11 @@ pub fn replayBatch(
             return .exit;
         }
 
-        const instruction_limit = svm_gateway.params.feature_set.active(
-            .static_instruction_limit,
-            svm_gateway.params.slot,
-        );
-        const instruction_accounts_limit = svm_gateway.params.feature_set.active(
-            .limit_instruction_accounts,
-            svm_gateway.params.slot,
-        );
         const hash, const compute_budget_details = switch (preprocessTransaction(
             transaction.transaction,
             .run_sig_verify,
-            instruction_limit,
-            instruction_accounts_limit,
+            &svm_gateway.params.feature_set,
+            svm_gateway.params.slot,
         )) {
             .ok => |res| res,
             .err => |err| return .{ .failure = err },
