40aed93038b81432c663df67f059fa7ac8bebcc0
Sobeston <15335529+Sobeston@users.noreply.github.com>
2026-05-21 05:45:35 +0800

    remove generic LCRS tree

--- v2/lib/collections.zig
+++ v2/lib/collections.zig
@@ -1,10 +1,8 @@
 comptime {
     if (@import("builtin").is_test) {
-        _ = @import("collections/lcrs_tree.zig");
         _ = @import("collections/pool.zig");
     }
 }
 
 pub const Pool = @import("collections/pool.zig").Pool;
-pub const LCRSTree = @import("collections/lcrs_tree.zig").LCRSTree;
 pub const SharedPool = @import("collections/pool.zig").SharedPool;
