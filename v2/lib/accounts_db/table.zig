50cd8d2911450f537343d221fcd8a242c6e6aa0b
kprotty <kbutcher6200@gmail.com>
2026-05-29 23:12:22 -0500

    accounts_db/table rewrite:
    - just store full pubkeys instead of their hash (dont have to worry about the extremely unlikely case of hash collision)
    - increase count to allow for >4B entries, granted enough memory
    - fix count increment for puts to empty ptr slots
    - make puts serial to handle the same pubkey in a batch
    - force a conditional store using `asm volatile`

--- v2/lib/accounts_db/table.zig
+++ v2/lib/accounts_db/table.zig
@@ -9,9 +9,19 @@ const FastDiv = lib.util.FastDiv;
 
 pub const Table = struct {
     seed: u64, // runtime-provided hashing seed (if wanting to help lower collision attacks)
-    count: u32, // can represent 4-billion accounts (would need re-design after that)
+    count: u64, // number of entries present in the table
     reducer: FastDiv, // for fast hash % entries.len
-    entries: []Entry,
+    entries: []Entry, // comes from []u8 so Entry must be align(1)
+
+    const Entry = extern struct {
+        key: Key align(1),
+        value: Value align(1),
+        slot: u32 align(1), // can represent another 50yrs of 400ms slots from current mainnet
+
+        comptime {
+            std.debug.assert(@alignOf(Entry) == 1);
+        }
+    };
 
     pub const Key = Pubkey;
     pub const Value = packed struct(u64) {
@@ -25,16 +35,10 @@ pub const Table = struct {
         }
     };
 
-    const Entry = extern struct { // 32-bytes, align(1) to ptrCast when stored in table.memory
-        hash: [16]u8, // compressed repr of Key (0 = empty, u128 should be enough for no collisions)
-        slot: u32 align(1), // can represent another 50yrs of 400ms slots from current mainnet
-        idx: u32 align(1), // intermediary state for hash % num_entries
-        value: Value align(1),
-    };
-
     pub fn init(seed: u64, memory: []u8) Table {
-        // NOTE: self.memory should already be zeroed from mmap
-        const entries = std.mem.bytesAsSlice(Entry, memory);
+        // NOTE: memory should already be zeroed from mmap
+        const len = @divFloor(memory.len, @sizeOf(Entry));
+        const entries: []Entry = @ptrCast(memory[0..len * @sizeOf(Entry)]);
         std.debug.assert(entries.len > 0);
 
         return .{
@@ -48,48 +52,11 @@ pub const Table = struct {
     // Number of hash map probes to do together with ILP to amortize DRAM cache-miss latency.
     const PARALLEL_SCAN = 8;
 
-    // Hashes the pubkey for table access (see Entry.hash doc-comment)
-    fn hashPubkey(self: *const Table, pubkey: *const Pubkey) u128 {
-        // based on xxh3 / rapidhash / wyhash mixing
-        const s: [4]u64 = .{
-            0xbe4ba423396cfeb8,
-            0x1cad21f72c81017c,
-            0xdb979083e96dd4de,
-            0x1f67b3b7a4a44072,
-        };
-        const seed = self.seed;
-        const in: [4]u64 = @bitCast(pubkey.data);
-        const a: [2]u64 = @bitCast(@as(u128, in[0] ^ (s[0] +% seed)) * (in[1] ^ (s[1] -% seed)));
-        const b: [2]u64 = @bitCast(@as(u128, in[2] ^ (s[2] +% seed)) * (in[3] ^ (s[3] -% seed)));
-        return @bitCast([2]u64{
-            (a[0] ^ a[1]) | 1, // make sure at least one bit is set so the result is non-zero
-            b[0] ^ b[1],
-        });
-    }
-
-    // hash % num_entries (based on xxh3 avalanche)
-    fn reduceHash(self: *const Table, hash: u128) u32 {
-        // merge both u64's in hash
-        var acc = @as(u64, 0x9E3779B185EBCA87) *% @sizeOf(@TypeOf(hash));
-        acc +%= @as([2]u64, @bitCast(hash))[0];
-        acc +%= @as([2]u64, @bitCast(hash))[1];
-
-        // avalanche across all bits
-        acc ^= acc >> 37;
-        acc *%= 0x165667919E3779F9;
-        acc ^= acc >> 32;
-
-        // reduce into item index
-        const acc_mod_len = acc - (self.reducer.div(acc) * self.entries.len);
-        std.debug.assert(acc_mod_len < self.entries.len);
-        return @intCast(acc_mod_len);
-    }
-
     pub const PutBatch = extern struct {
         len: u32,
-        entries: [PARALLEL_SCAN]Entry,
+        items: [PARALLEL_SCAN]Entry,
 
-        pub const empty: PutBatch = .{ .len = 0, .entries = undefined };
+        pub const empty: PutBatch = .{ .len = 0, .items = undefined };
     };
 
     pub fn put(
@@ -99,22 +66,22 @@ pub const Table = struct {
         slot: Slot,
         value: Value,
     ) void {
+        std.debug.assert(!pubkey.isZeroed());
         std.debug.assert(slot <= std.math.maxInt(u32));
         std.debug.assert(!value.isEmpty());
 
-        const hash = self.hashPubkey(pubkey);
-        std.debug.assert(hash != 0);
+        std.debug.assert(batch.len <= PARALLEL_SCAN);
+        if (batch.len == PARALLEL_SCAN) {
+            @branchHint(.unlikely);
+            self.flushPuts(batch);
+        }
 
-        std.debug.assert(batch.len < PARALLEL_SCAN);
-        batch.entries[batch.len] = .{
-            .hash = @bitCast(hash),
-            .slot = @intCast(slot),
-            .idx = self.reduceHash(hash),
+        batch.items[batch.len] = .{
+            .key = pubkey.*,
             .value = value,
+            .slot = @intCast(slot),
         };
-
         batch.len += 1;
-        if (batch.len == PARALLEL_SCAN) self.flushPuts(batch);
     }
 
     pub fn flushPuts(noalias self: *Table, noalias batch: *PutBatch) void {
@@ -124,91 +91,133 @@ pub const Table = struct {
         const batch_len = batch.len;
         std.debug.assert(batch_len <= PARALLEL_SCAN);
         if (batch_len == 0) return;
-        batch.len = 0; // mark batch as flushed
+        batch.len = 0; // mark as flushed
 
-        // phase-0: incomplete batch entries are zeroed out
+        // table should never fill up fully (means it shouldve been given more memory)
+        std.debug.assert(self.count + batch_len < self.entries.len);
+
+        // phase-0: zero out unused batch items
+        const zero: @Vector(32, u8) = @splat(0);
+        var item_keys: [PARALLEL_SCAN]@Vector(32, u8) = undefined;
         for (0..PARALLEL_SCAN) |i| {
-            const mask: @Vector(32, bool) = @splat(i < batch_len);
-            const put_vec: @Vector(32, i8) = @bitCast(batch.entries[i]); // i8 cmp has better codegen
-            batch.entries[i] = @bitCast(@select(i8, mask, put_vec, put_vec - put_vec)); // x-x=0
+            const valid: @Vector(32, bool) = @splat(i < batch_len);
+            const key: @Vector(32, u8) = @bitCast(batch.items[i].key);
+            item_keys[i] = @bitCast(@select(u8, valid, key, zero)); // valid ? key : 0
         }
 
-        // phase-1: gather map pointers
+        // phase-1: hash pubkeys into table entry pointers
         var ptrs: [PARALLEL_SCAN][*]Entry = undefined;
         for (0..PARALLEL_SCAN) |i| {
-            ptrs[i] = self.entries.ptr + batch.entries[i].idx;
+            const pubkey: Pubkey = @bitCast(item_keys[i]);
+            ptrs[i] = self.entries.ptr + self.hashKeyToIndex(&pubkey);
         }
-
+        
         // phase-2: parallel-loads of entries
-        var curr: [PARALLEL_SCAN]@Vector(32, u8) = undefined;
+        var keys: [PARALLEL_SCAN]@Vector(32, u8) = undefined;
         for (0..PARALLEL_SCAN) |i| {
-            curr[i] = @bitCast(ptrs[i][0]); // start load of entry
-            @prefetch(@as([*]u8, @ptrCast(ptrs[i])) + 64, .{}); // start fetching next probe cache line
+            keys[i] = @bitCast(ptrs[i][0].key);
         }
 
         // phase-3: parallel-probe of entries
-        std.debug.assert(self.count < self.entries.len);
         while (true) {
-            var has_collisions = false;
+            var had_collision = false;
             for (0..PARALLEL_SCAN) |i| {
-                const put_hash: @Vector(16, u8) = batch.entries[i].hash;
-                const curr_hash = @as([2]@Vector(16, u8), @bitCast(curr[i]))[0];
-
-                const zero: @Vector(16, u8) = @splat(0);
-                const empty_mask = @select(u8, curr_hash == zero, ~zero, zero);
-                const eq_mask = @select(u8, curr_hash == put_hash, ~zero, zero);
+                const item_key = item_keys[i];
+                const empty_mask = @select(u8, keys[i] == zero, ~zero, zero);
+                const eq_mask = @select(u8, keys[i] == item_key, ~zero, zero);
 
                 // check for collisions (!eq and !empty = populated with unrelated entry)
                 const collided = @reduce(.Or, (empty_mask | eq_mask) == zero);
-                if (collided) has_collisions = true;
+                if (collided) had_collision = true;
 
                 // probe next item if collided
                 ptrs[i] += @intFromBool(collided);
                 if (ptrs[i] == self.entries.ptr + self.entries.len) ptrs[i] = self.entries.ptr;
-                curr[i] = @bitCast(ptrs[i][0]);
+                keys[i] = @bitCast(ptrs[i][0].key);
             }
-            if (!has_collisions) break;
+            if (!had_collision) break;
         }
 
-        // phase-4: insert into entries that are empty or have lower slots
+        // phase-4: do the updates
         var stub: Entry = undefined;
         for (0..PARALLEL_SCAN) |i| {
-            // empty curr has .slot=0 : always overwritten
-            const newer_slot = @as(Entry, @bitCast(curr[i])).slot <= batch.entries[i].slot;
+            const item_slot = batch.items[i].slot;
+            const newer_slot = @intFromBool(item_slot >= ptrs[i][0].slot);
+            
+            const item_key: Pubkey = @bitCast(item_keys[i]);
+            const valid = @intFromBool(!item_key.isZeroed());
 
-            const new_write = newer_slot and batch.entries[i].slot > 0;
-            self.count += @intFromBool(new_write); // bump for empty inserts
+            const overwrite = valid & newer_slot;
+            const was_empty = @intFromBool(ptrs[i][0].key.isZeroed());
+            self.count += overwrite & was_empty;
 
-            const ptr = if (newer_slot) &ptrs[i][0] else &stub;
-            ptr.* = batch.entries[i];
+            var dst = if (overwrite > 0) &ptrs[i][0] else &stub;
+            asm volatile("" : [_] "=r" (dst) : [_] "r" (dst)); // hide from compiler to not branch
+
+            dst.* = .{
+                .key = item_key,
+                .slot = item_slot,
+                .value = batch.items[i].value,
+            };
         }
-
-        // Hard cap at this point to consider a re-design (store Pubkey directly, chain probing)
-        std.debug.assert(self.count < std.math.maxInt(u32));
     }
 
     pub fn get(self: *const Table, pubkey: *const Pubkey) Value {
-        const hash = self.hashPubkey(pubkey);
-        std.debug.assert(hash != 0);
+        std.debug.assert(!pubkey.isZeroed());
 
-        var idx = self.reduceHash(hash);
-        for (0..self.entries.len) |_| {
-            const e = &self.entries[idx];
-            idx +%= 1;
-            if (idx >= self.entries.len) idx = 0; // cmov
+        var ptr = self.entries.ptr + self.hashKeyToIndex(pubkey);
+        while (true) {
+            // load the entry values out
+            const key = ptr[0].key;
+            const value = ptr[0].value;
 
-            const e_hash: u128 = @bitCast(e.hash);
-            if (e_hash == 0) {
-                @branchHint(.unlikely); // looking up non-existing accounts is rare
+            // branchless increment
+            ptr += 1;
+            if (ptr == self.entries.ptr + self.entries.len) ptr = self.entries.ptr;
+
+            // branch out for empty (looking up unfound accounts is rare)
+            if (key.isZeroed()) {
+                @branchHint(.unlikely);
                 return .empty;
             }
 
-            if (e_hash == hash) {
-                std.debug.assert(!e.value.isEmpty());
-                return e.value;
+            // branch back to loop on collision (entry, but not matching one)
+            if (key.equals(pubkey)) {
+                std.debug.assert(!value.isEmpty());
+                return value;
             }
         }
+    }
 
-        unreachable; // table is full?
+    fn hashKeyToIndex(self: *const Table, pubkey: *const Pubkey) u64 {
+        // based on xxh3 / rapidhash / wyhash mixing
+        const sd = self.seed;
+        const sk: [4]u64 = .{
+            0xbe4ba423396cfeb8,
+            0x1cad21f72c81017c,
+            0xdb979083e96dd4de,
+            0x1f67b3b7a4a44072,
+        };
+        
+        // XXH3_mix16B
+        const in: [4]u64 = @bitCast(pubkey.*);
+        const a: [2]u64 = @bitCast(@as(u128, in[0] ^ (sk[0] +% sd)) * (in[1] ^ (sk[1] -% sd)));
+        const b: [2]u64 = @bitCast(@as(u128, in[2] ^ (sk[2] +% sd)) * (in[3] ^ (sk[3] -% sd)));
+        
+        // XXH3_len_17to128_64b
+        var acc = @as(u64, 0x9E3779B185EBCA87) *% 32;
+        acc +%= a[0] ^ a[1];
+        acc +%= b[0] ^ b[1];
+
+        // XXH3_avalanche
+        acc ^= acc >> 37;
+        acc *%= 0x165667919E3779F9;
+        acc ^= acc >> 32;
+
+        // reduce into table index
+        const acc_mod_len = acc - (self.reducer.div(acc) * self.entries.len);
+        std.debug.assert(acc_mod_len < self.entries.len);
+        return acc_mod_len;
     }
 };
+
