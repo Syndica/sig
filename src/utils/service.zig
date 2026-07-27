5aa81e8649ffc5fe5043d7edd3ab3e6c00006916
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2026-07-27 07:19:50 -0400

    chore: rollback changes on buf at `runService`

--- src/utils/service.zig
+++ src/utils/service.zig
@@ -176,8 +176,7 @@ pub fn runService(
         }
     };
 
-    // adding 7 for the "thread " prefix
-    var buf: [7 + std.Thread.max_name_len]u8 = undefined;
+    var buf: [16]u8 = undefined;
     const name = maybe_name orelse try std.fmt.bufPrint(
         &buf,
         "thread {d}",
